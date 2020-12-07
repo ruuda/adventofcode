896c2de788102ad183a5d80d5fcd30bb6da5d1f6
Ruud van Asseldonk <dev@veniogames.com>
2020-12-07 21:38:58 +0100

    Solve day 7 part 2

--- 07/main.py
+++ 07/main.py
@@ -31,8 +31,11 @@ def parse(line: str) -> Bag:
 
 
 outers: Dict[str, Set[str]] = defaultdict(lambda: set())
+bags: Dict[str, Dict[str, int]] = {}
+
 for line in open('input.txt', 'r', encoding='ascii'):
     bag = parse(line.strip())
+    bags[bag.outer_color] = bag.inner
     for inner in bag.inner.keys():
         outers[inner].add(bag.outer_color)
 
@@ -47,3 +50,9 @@ while len(frontier) > 0:
     valid = new_valid
 
 print(len(valid))
+
+def num_bags(color: str) -> int:
+    return 1 + sum(n * num_bags(inner) for inner, n in bags[color].items())
+
+# Subtract 1, because the outer bag does not count itself.
+print(num_bags('shiny gold') - 1)
