4d612b2b49f1dea26d57a65a19bd8f7c1fc32c1a
Ruud van Asseldonk <dev@veniogames.com>
2020-12-08 22:50:09 +0100

    Solve day 8 part 2

--- 08/main.hs
+++ 08/main.hs
@@ -17,17 +17,41 @@ parseInstr s =
       "jmp" -> Jmp arg
       "nop" -> Nop arg
 
-accumulatorBeforeFirstLoop :: [Instr] -> Int
-accumulatorBeforeFirstLoop program = go 0 0 []
+data Result
+  = Terminates Int
+  | Loops Int
+  deriving (Show)
+
+-- Return the accumulator at the end of the program, or before it loops.
+executeProgram :: [Instr] -> Result
+executeProgram program = go 0 0 []
   where
     go !acc !pc !visited =
       case program !! pc of
-        _ | pc `elem` visited -> acc
+        _ | pc `elem` visited -> Loops acc
+        _ | pc == length program -> Terminates acc
         Acc n -> go (acc + n) (pc + 1) (pc : visited)
         Jmp z -> go acc (pc + z) (pc : visited)
         Nop _ -> go acc (pc + 1) (pc : visited)
 
+flipAt :: [Instr] -> Int -> [Instr]
+flipAt program i = fmap flipInstr $ zip [0..] program
+  where
+    flipInstr p = case p of
+      (j, (Jmp z)) | i == j -> Nop z
+      (j, (Nop z)) | i == j -> Jmp z
+      (_, instr)            -> instr
+
+flips :: [Instr] -> [[Instr]]
+flips program = fmap (flipAt program) (take (length program) [0..])
+
+firstTermination :: [Result] -> Result
+firstTermination results = case results of
+  (Loops _ : rs)     -> firstTermination rs
+  (Terminates z : _) -> Terminates z
+
 main :: IO ()
 main = do
   program <- fmap parseInstr <$> lines <$> readFile "input.txt"
-  putStrLn $ show $ accumulatorBeforeFirstLoop program
+  putStrLn $ "Part 1: " <> (show $ executeProgram program)
+  putStrLn $ "Part 2: " <> (show $ firstTermination $ fmap executeProgram $ flips program)
