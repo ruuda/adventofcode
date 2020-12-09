96bb68dc17e8c472aef642cd1465e8eab7cba88c
Ruud van Asseldonk <dev@veniogames.com>
2020-12-09 23:01:45 +0100

    Solve day 9 part 2

--- 09/main.py
+++ 09/main.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 
 past = []
+bad_num: int
 
-for line in open('input.txt', 'r', encoding='ascii'):
-    n = int(line)
+numbers = [int(line) for line in open('input.txt', 'r', encoding='ascii')]
 
+# Part 1
+for n in numbers:
     is_ok = False
 
     for k in past:
@@ -14,7 +16,17 @@ for line in open('input.txt', 'r', encoding='ascii'):
             break
 
     if not is_ok and len(past) == 25:
+        bad_num = n
         print(n)
 
     past.append(n)
     past = past[-25:]
+
+# Part 2
+for i, n in enumerate(numbers):
+    s = 0
+    for j, k in enumerate(numbers[i:]):
+        s += k
+        if s == bad_num and j > 0:
+            rg = numbers[i:i + j]
+            print(min(rg) + max(rg))
