a283e47db70022d5e3d7cb9b716ec1ea2f7364cc
Ruud van Asseldonk <dev@veniogames.com>
2021-12-06 23:56:49 +0100

    Add alternative solution for day 1 part 2

--- 2021/01/part2.hs
+++ 2021/01/part2.hs
@@ -14,5 +14,14 @@ solve xs =
   in
     length $ filter (\(x, y) -> y > x) (zip xs3 $ tail xs3) 
 
+-- An alternative definition of "solve" that is a bit more low-level but also
+-- less cryptic and a bit shorter depending on how you count.
+solveAlt :: [Int] -> Int
+solveAlt = solveAlt' 0
+  where
+    solveAlt' n (a:b:c:d:more) | a + b + c < b + c + d = solveAlt' (n + 1) (b:c:d:more)
+    solveAlt' n (_:b:c:d:more) | otherwise             = solveAlt' n       (b:c:d:more)
+    solveAlt' n _ = n
+
 main :: IO ()
 main = readFile "input.txt" >>= print . solve . fmap read . lines
