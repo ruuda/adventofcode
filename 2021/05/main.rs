d00a3f055f94031e37dedce4b0a0839b2c88c8fb
Ruud van Asseldonk <dev@veniogames.com>
2021-12-14 21:14:02 +0100

    Compute line intersections, but fail
    This also includes some squashed work in progress that I abandoned.

--- 2021/05/main.rs
+++ 2021/05/main.rs
@@ -5,63 +5,99 @@ use std::io::{BufRead, BufReader};
 use std::io;
 use std::str::FromStr;
 
-/// An axis-aligned line.
-#[derive(Copy, Clone, Debug)]
-struct AaLine { x: u32, y: u32, len: u32 }
-
-fn report_horizontal_intersections<X: Fn(AaLine) -> u32, Y: Fn(AaLine) -> u32>(
-    lines: &[AaLine],
-    px: X,
-    py: Y,
-) -> HashSet<(u32, u32)> {
-    let mut out = HashSet::new();
-
-    'outer: for i in 0..lines.len() {
-        for j in (i + 1)..lines.len() {
-            let z0 = lines[i];
-            let z1 = lines[j];
-
-            if py(z0) != py(z1) {
-                // These lines are not at the same y-coordinate, they surely do
-                // not overlap.
-                continue 'outer;
-            }
-
-            if px(z1) <= px(z0) + z0.len {
-                // The lines overlap, and z0 is left of z1. The reverse case
-                // does not happen, because our input is sorted.
-                for x in px(z1)..cmp::min(px(z0) + z0.len, px(z1) + z1.len) {
-                    out.insert((x, py(z0)));
-                }
-            }
-        }
-    }
-
-    out
+/// A line segment defined by a direction vector (dx, dy), a starting point
+/// (x, y) on the line, and the length of the segment.
+///
+/// Note, the direction vector does not need to be a unit vector, but for the
+/// purpose of checking intersections, we do assume that all directions have one
+/// unique representation.
+#[derive(Debug)]
+struct Line {
+    x: i32,
+    y: i32,
+    dx: i32,
+    dy: i32,
+    len: i32,
 }
 
-/// Report intersections between horizontal and vertical lines.
-fn report_intersections(hlines: &[AaLine], vlines: &[AaLine], out: &mut HashSet<(u32, u32)>) {
-    for zh in hlines {
-        for zv in vlines {
-            // We could take advantage of the vlines being sorted to early out
-            // here, but for now quadratic complexity is acceptable.
-            if zh.y < zv.y || zh.y >= zv.y + zv.len {
-                continue
+impl Line {
+    fn intersect(&self, other: &Line, out: &mut HashSet<(i32, i32)>) {
+        let is_parallel =
+            (self.dx * other.dx + self.dy * other.dy).abs() ==
+            (self.dx.abs() + self.dy.abs());
+
+        if is_parallel {
+            // Case 1: The lines are parallel. Check if they overlap.
+            let perp0 = self.dy * self.x - self.dx * self.y;
+            let perp1 = other.dy * other.x - other.dx * other.y;
+            if perp0 != perp1 {
+                return // The lines do not overlap.
             }
-            if zv.x < zh.x || zv.x >= zh.x + zh.len {
-                continue
+
+            // The lines do overlap, but do the line segments overlap?
+            let par0 = self.dx * self.x + self.dy * self.y;
+            let par1 = other.dx * other.x + other.dy * other.y;
+            let t0 = cmp::max(par0, par1);
+            let t1 = cmp::min(par0 + self.len, par1 + other.len);
+            for t in t0..t1 {
+                let x = self.dx * t + self.dy * perp0;
+                let y = self.dy * t - self.dx * perp0;
+                out.insert((x, y));
             }
-            out.insert((zv.x, zh.y));
+        } else {
+            // Case 2: The lines are not parallel, therefore they intersect in
+            // one unique point. (Which may not lie inside the line segment
+            // though).
+
+            // If we translate everything such that `self.xy` is at (0, 0), and
+            // then parametrize `other` as `Q(t) = other.xy + t * other.dxy`,
+            // then the point at which `dot(Q(t), normal(self.xy)) = 0` is the
+            // point where the two lines (but maybe not line segments)
+            // intersect. We can solve for `t`, and if it lies within other's
+            // length, the intersection lies at least on that line segement.
+
+            // Q(t1) = (b.x + b.dx * t1).
+            //    dot(Q(t1), normal(a.dx)) = 0
+            // => dot(b.x + b.dx * t1, na) = 0
+            // => dot(b.x, na) + t1 * dot(b.dx, na) = 0
+            // t1 = - dot(b.x, na) / dot(b.dx, na)
+
+            let px = other.x - self.x;
+            let py = other.y - self.y;
+            let dot_x = py * self.dx - px * self.dy;
+            let dot_dx = other.dy * self.dx - other.dx * self.dy;
+            let t = -dot_x / dot_dx;
+            if t < 0 || t > other.len {
+                // Intersection does not lie on this line segement.
+                return
+            }
+
+            let x = other.x + other.dx * t;
+            let y = other.y + other.dy * t;
+
+            // Now we flip the role of `self` and `other` and do the same.
+            let px = self.x - other.x;
+            let py = self.y - other.y;
+            let dot_x = py * other.dx - px * other.dy;
+            let dot_dx = self.dy * other.dx - self.dx * other.dy;
+            let t = -dot_x / dot_dx;
+            if t < 0 || t > self.len {
+                // Intersection does not lie on this line segement.
+                return
+            }
+
+            debug_assert_eq!(x, self.x + self.dx * t);
+            debug_assert_eq!(y, self.y + self.dy * t);
+
+            out.insert((x, y));
         }
     }
 }
 
 fn main() -> io::Result<()> {
-    let mut hlines = Vec::new();
-    let mut vlines = Vec::new();
+    let mut lines = Vec::new();
 
-    for file_line_io in BufReader::new(File::open("input.txt")?).lines() {
+    for file_line_io in BufReader::new(File::open("test.txt")?).lines() {
         let file_line = file_line_io?;
 
         let mut coords = file_line.split(" -> ");
@@ -73,50 +109,50 @@ fn main() -> io::Result<()> {
             .next()
             .expect("Need two coords separated by ' -> '.")
             .split(",");
-        let x0 = u32::from_str(c0.next().expect("Need a coordinate lin 'x,y' format.")).expect("Need integer coordinates");
-        let y0 = u32::from_str(c0.next().expect("Need a coordinate lin 'x,y' format.")).expect("Need integer coordinates");
-        let x1 = u32::from_str(c1.next().expect("Need a coordinate lin 'x,y' format.")).expect("Need integer coordinates");
-        let y1 = u32::from_str(c1.next().expect("Need a coordinate lin 'x,y' format.")).expect("Need integer coordinates");
+        let x0 = i32::from_str(c0.next().expect("Syntax error")).expect("Need integer coordinates");
+        let y0 = i32::from_str(c0.next().expect("Syntax error")).expect("Need integer coordinates");
+        let x1 = i32::from_str(c1.next().expect("Syntax error")).expect("Need integer coordinates");
+        let y1 = i32::from_str(c1.next().expect("Syntax error")).expect("Need integer coordinates");
 
-        if x0 == x1 {
-            if y1 > y0 {
-                vlines.push(AaLine { x: x0, y: y0, len: 1 + y1 - y0 });
-            } else {
-                vlines.push(AaLine { x: x0, y: y1, len: 1 + y0 - y1 });
-            }
-        } else if y0 == y1 {
-            if x1 > x0 {
-                hlines.push(AaLine { x: x0, y: y0, len: 1 + x1 - x0 });
-            } else {
-                hlines.push(AaLine { x: x1, y: y0, len: 1 + x0 - x1 });
+        /*if x0 > x1 {
+            std::mem::swap(&mut x0, &mut x1);
+            std::mem::swap(&mut y0, &mut y1);
+        }*/
+        let dx = (x1 - x0).signum();
+        let dy = (y1 - y0).signum();
+
+        let z = if dx.abs() + dy.abs() > 0 {
+            Line {
+                x: x0, y: y0,
+                dx, dy,
+                len: 1 + ((y1 - y0).abs() + (x1 - x0).abs()) / (dx.abs() + dy.abs())
             }
         } else {
-            // We ignore non-axis-aligned lines in part 1.
+            // If it's one cell, it doesn't have any direction in particular,
+            // pick one.
+            Line {
+                x: x0, y: y0,
+                dx: 1, dy: 0,
+                len: 1,
+            }
+        };
+        lines.push(z);
+    }
+    for z in lines.iter() {
+        println!("{:?}", z);
+    }
+
+    let mut intersections = HashSet::new();
+
+    for (i, z) in lines.iter().enumerate() {
+        for (_, w) in lines[i + 1..].iter().enumerate() {
+            z.intersect(&w, &mut intersections);
         }
     }
-
-    // Sort the lines so we can efficiently find overlap.
-    hlines.sort_by_key(|z| (z.y, z.x, z.len));
-    vlines.sort_by_key(|z| (z.x, z.y, z.len));
-
-    // Start by computing direct overlap between the lines. The function is
-    // written from the horizontal point of view, for vertical we swap the
-    // coordinates.
-    let h_isects = report_horizontal_intersections(&hlines[..], |z| z.x, |z| z.y);
-    let v_isects = report_horizontal_intersections(&vlines[..], |z| z.y, |z| z.x);
-
-    // Take the union of the two sets of points. Swap back the x and y for the
-    // vertical intersections.
-    let mut isects = h_isects;
-    for (y, x) in v_isects {
-        isects.insert((x, y));
-    }
-
-    // Now aside from the intersections between lines of the same orientation,
-    // find intersections between different orientations.
-    report_intersections(&hlines[..], &vlines[..], &mut isects);
-
-    println!("{}", isects.len());
+    let mut is: Vec<_> = intersections.iter().collect();
+    is.sort();
+    println!("{:?}", is);
+    println!("{}", intersections.len());
 
     Ok(())
 }
