a9908b35e72160e02ab4c3584bf30b1285791672
Ruud van Asseldonk <dev@veniogames.com>
2022-11-30 20:56:56 +0100

    Add development environment with Unison tools
    I want to try out Unison this year. Also Hoon. Let's prepare by adding
    the required tools, Unison first.

---
+++ 2022/flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "Advent of Code 2022 devenv";
+
+  inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
+  inputs.unison.url = "github:ceedubs/unison-nix";
+
+  outputs = { self, nixpkgs, unison }: 
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+      {
+        devShells.${system}.default = pkgs.mkShell {
+          name = "aoc2022";
+          nativeBuildInputs = [
+            unison.packages.${system}.ucm
+          ];
+        };
+      };
+}
