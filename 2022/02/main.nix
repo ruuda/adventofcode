fb01524dc108f9782e929ea8f5ea7809dd8fe201
Ruud van Asseldonk <dev@veniogames.com>
2022-12-02 20:58:39 +0100

    Solve 2022-02b

--- 2022/02/main.nix
+++ 2022/02/main.nix
@@ -29,13 +29,31 @@ let
 
   # Determine the outcome of a vs. b:
   # 1: a wins, 0: draw, -1: b wins.
-  outcome = a: b: mod3 (asNumber a - asNumber b);
+  outcome = a: b: mod3 (a - b);
 
   # The value of a move that we play (1 for rock/x, 3 for scissors/z).
-  shapeScore = moves: 1 + asNumber (selfMove moves);
-  outcomeScore = moves: 3 - 3 * (outcome (opponentMove moves) (selfMove moves));
-  score = move: (shapeScore move) + (outcomeScore move);
+  shapeScore = a: b: 1 + b;
+  outcomeScore = a: b: 3 - 3 * (outcome a b);
+  score = a: b: (shapeScore a b) + (outcomeScore a b);
+
+  # Parse the line into numeric moves a and b, then call f(a, b).
+  ap2 = f: moves: f (asNumber (opponentMove moves)) (asNumber (selfMove moves));
 
   sum = builtins.foldl' builtins.add 0;
+
+  answerPartA = sum (builtins.map (ap2 score) lines);
+
+  # Like mod3, but put the result in [0, 2] instead of [-1, 1].
+  mod3' = x:
+    if x > 2
+    then mod3' (x - 3)
+    else (if x < 0 then mod3' (x + 3) else x);
+
+  # Get the move to play according to the instruction, which is 0 for losing,
+  # 1 for a draw, and 2 for winning.
+  getMove = a: instr: mod3' (a - 1 + instr);
+
+  scoreB = a: instr: score a (getMove a instr);
+  answerPartB = sum (builtins.map (ap2 scoreB) lines);
 in
-  sum (builtins.map score lines)
+  answerPartB
