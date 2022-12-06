c17c059eb21985c997b019715dad382aa8ea7369
Ruud van Asseldonk <dev@veniogames.com>
2022-12-06 11:10:16 +0100

    Solve 2022-06 part 2 as well

--- 2022/06/main.py
+++ 2022/06/main.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 
-def index_first_four_different(datastream: str) -> int:
-    for i in range(4, len(datastream)):
-        if len(set(datastream[i - 4:i])) == 4:
+def index_first_n_different(n: int, datastream: str) -> int:
+    for i in range(n, len(datastream)):
+        if len(set(datastream[i - n:i])) == n:
             return i
 
     raise Exception("No four consecutive characters are different.")
 
 datastream = open("input.txt", "r", encoding="utf-8").read().strip()
-print(index_first_four_different(datastream))
+print("Part 1 answer:", index_first_n_different(4, datastream))
+print("Part 2 answer:", index_first_n_different(14, datastream))
