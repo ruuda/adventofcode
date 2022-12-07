36b5d5f67b5506d15da540c5f996e973e04cc378
Ruud van Asseldonk <dev@veniogames.com>
2022-12-07 10:13:32 +0100

    Build interpreter for 2022-07
    This does not yet compute the answer, but at least I can read the
    directory structure. The example looks correct.

---
+++ 2022/07/main.hs
@@ -0,0 +1,84 @@
+#!/usr/bin/env runhaskell
+
+{-# LANGUAGE LambdaCase #-}
+
+module Main where
+
+import Control.Monad (foldM)
+import Data.Map (Map)
+import Prelude hiding (appendFile)
+
+import qualified Data.Map as Map
+
+data Dir = Dir
+  { subdirs :: Map String Dir
+  , files :: Map String Int
+  } deriving (Show)
+
+emptyDir = Dir Map.empty Map.empty
+
+alter :: (Maybe Dir -> Maybe Dir) -> [String] -> Dir -> Dir
+alter f path self = case path of
+  [] -> case f (Just self) of
+    Just dir -> dir
+    Nothing  -> emptyDir
+  part : more ->
+    self { subdirs = Map.alter (fmap (alter f more)) part (subdirs self) }
+
+showDir :: Dir -> [String]
+showDir dir =
+  let
+    showFile (fname, size) = fname <> "\t(file, size=" <> (show size) <> ")"
+    showSubdir (dirname, contents) = [dirname <> "\t(dir)"] <> (fmap ("  " <>) (showDir contents))
+    fileLines = fmap showFile $ Map.toList $ files dir
+    dirLines = concatMap showSubdir $ Map.toList $ subdirs dir
+  in
+    fileLines <> dirLines
+
+data State = State 
+  { statePath :: [String] 
+  , stateRoot :: Dir
+  } deriving (Show)
+
+emptyState = State [] emptyDir
+
+alterCurrent :: (Dir -> Dir) -> State -> State
+alterCurrent f state =
+  let
+    f' Nothing    = Just $ f emptyDir
+    f' (Just dir) = Just $ f dir
+  in
+    state { stateRoot = alter f' (statePath state) (stateRoot state) }
+
+appendDir :: String -> Dir -> Dir
+appendDir dirname dir = dir
+  { subdirs = Map.insert dirname emptyDir (subdirs dir) }
+
+appendFile :: String -> Int -> Dir -> Dir
+appendFile fname size dir = dir
+  { files = Map.insert fname size (files dir) }
+
+execute :: String -> State -> State
+execute cmd state = case words cmd of
+  ["$", "cd", "/"]  -> state { statePath = [] }
+  ["$", "cd", ".."] -> state { statePath = init $ statePath state }
+  ["$", "cd", dir]  -> state { statePath = (statePath state) ++ [dir] }
+  ["$", "ls"]       -> state
+  ["dir", dirname]  -> alterCurrent (appendDir dirname) state
+  [size, fname]     -> alterCurrent (appendFile fname (read size)) state
+
+main :: IO ()
+main = do
+  fileContents <- lines <$> readFile "example.txt"
+  finalState <- foldM
+    (\state line -> do
+      putStrLn $ "pre: " <> show state
+      putStrLn $ unlines $ showDir $ stateRoot state
+      putStrLn $ "cmd: " <> line
+      pure $ execute line state
+    )
+    emptyState
+    fileContents
+
+  putStrLn $ show finalState
+  putStrLn $ unlines $ showDir $ stateRoot finalState
