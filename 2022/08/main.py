54ef4e27de19d4d17eabc48cc60c817182eaf128
Ruud van Asseldonk <dev@veniogames.com>
2022-12-08 08:52:05 +0100

    Solve 2022-08 part 2

--- 2022/08/main.py
+++ 2022/08/main.py
@@ -29,5 +29,32 @@ vis_y_pos = iter_visible(1, 0, [(0, x) for x in range(w)])
 vis_y_neg = iter_visible(-1, 0, [(h - 1, x) for x in range(w)])
 
 all_visible = set(vis_x_pos) | set(vis_x_neg) | set(vis_y_pos) | set(vis_y_neg)
+print("Part 1:", len(all_visible))
 
-print(len(all_visible))
+
+def viewing_distance(p: Point, dy: int, dx: int) -> int:
+    y, x = p
+    n = 0
+    z = grid[y][x]
+    while True:
+        y += dy
+        x += dx
+        if y < 0 or x < 0 or y >= h or x >= w:
+            return n
+        n += 1
+        if grid[y][x] >= z:
+            return n
+
+
+def scenic_score(p: Point) -> int:
+    return (
+        1
+        * viewing_distance(p, -1, 0)
+        * viewing_distance(p, 0, -1)
+        * viewing_distance(p, 0, 1)
+        * viewing_distance(p, 1, 0)
+    )
+
+
+max_score = max(scenic_score((y, x)) for y in range(h) for x in range(w))
+print("Part 2:", max_score)
