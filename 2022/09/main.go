936b4353fce686916f27eb2d9a2bac51e630ff32
Ruud van Asseldonk <dev@veniogames.com>
2022-12-09 08:40:30 +0100

    Solve 2022-09 part 1 in Go
    I did not think I would like Go, and so far I do not like it. But I'm
    trying to keep an open mind ...

--- 2022/09/main.go
+++ 2022/09/main.go
@@ -13,10 +13,40 @@ func check(e error) {
 	}
 }
 
+type Point struct {
+  x, y int
+}
+
+// Lol no generics, how is this not in the stdlib?
+func absInt(x int) int {
+  if x < 0 {
+    return -x
+  } else {
+    return x
+  }
+}
+
+func sgnInt(x int) int {
+  if x < 0 {
+    return -1
+  } else if x > 0 {
+    return 1
+  } else {
+    return 0
+  }
+}
+
 func main() {
-	data, err := os.ReadFile("example.txt")
+	data, err := os.ReadFile("input.txt")
 	check(err)
 	lines := strings.Split(string(data), "\n")
+
+  headPos := Point{0, 0}
+  tailPos := Point{0, 0}
+  
+  // Lol, no built-in set data structure.
+  tailPositions := map[Point]int{tailPos: 1}
+
 	for _, line := range lines {
     if line == "" {
       continue
@@ -24,6 +54,47 @@ func main() {
     direction := line[:1]
     distance, err := strconv.Atoi(line[2:])
     check(err)
-		fmt.Printf("%s %d\n", direction, distance)
+
+    for i := 0; i < distance; i++ {
+      // Move the head.
+      switch direction {
+      case "U":
+        headPos.y += 1
+      case "D":
+        headPos.y -= 1
+      case "R":
+        headPos.x += 1
+      case "L":
+        headPos.x -= 1
+      }
+
+      // The tail moves diagonally if it is not within distance 1 of the head.
+      dx := headPos.x - tailPos.x
+      dy := headPos.y - tailPos.y
+
+      if absInt(dx) <= 1 && absInt(dy) <= 1 {
+        // Don't move, the head and tail are already touching.
+      } else if absInt(dx) == 0 {
+        // Already in the same column, move only within the column.
+        tailPos.y += sgnInt(dy)
+      } else if absInt(dy) == 0 {
+        // ALready in the same row, move only within the row.
+        tailPos.x += sgnInt(dx)
+      } else {
+        // We need to do a diagonal move.
+        tailPos.x += sgnInt(dx)
+        tailPos.y += sgnInt(dy)
+      }
+
+      tailPositions[tailPos] = 1
+
+		fmt.Printf(
+      "%s %d/%d -> H(%d, %d) T(%d, %d), %d unique positions\n",
+      direction, i, distance,
+      headPos.x, headPos.y,
+      tailPos.x, tailPos.y,
+      len(tailPositions),
+    )
+    }
 	}
 }
