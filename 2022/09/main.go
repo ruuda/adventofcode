60bbcfa9baebc35faed8357dc4ffa136a47c48b2
Ruud van Asseldonk <dev@veniogames.com>
2022-12-09 08:59:34 +0100

    Gofmt the entire thing
    Tabs, awful.

--- 2022/09/main.go
+++ 2022/09/main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"os"
 	"fmt"
+	"os"
+	"strconv"
 	"strings"
-  "strconv"
 )
 
 func check(e error) {
@@ -14,26 +14,26 @@ func check(e error) {
 }
 
 type Point struct {
-  x, y int
+	x, y int
 }
 
 // Lol no generics, how is this not in the stdlib?
 func absInt(x int) int {
-  if x < 0 {
-    return -x
-  } else {
-    return x
-  }
+	if x < 0 {
+		return -x
+	} else {
+		return x
+	}
 }
 
 func sgnInt(x int) int {
-  if x < 0 {
-    return -1
-  } else if x > 0 {
-    return 1
-  } else {
-    return 0
-  }
+	if x < 0 {
+		return -1
+	} else if x > 0 {
+		return 1
+	} else {
+		return 0
+	}
 }
 
 func main() {
@@ -41,73 +41,73 @@ func main() {
 	check(err)
 	lines := strings.Split(string(data), "\n")
 
-  var currentPos [10]Point
-  // Lol, no built-in set data structure.
-  var uniquePos [10]map[Point]int
+	var currentPos [10]Point
+	// Lol, no built-in set data structure.
+	var uniquePos [10]map[Point]int
 
-  for i := 0; i < 10; i++ {
-    currentPos[i] = Point{0, 0}
-    uniquePos[i] = map[Point]int{currentPos[i]: 1}
-  }
+	for i := 0; i < 10; i++ {
+		currentPos[i] = Point{0, 0}
+		uniquePos[i] = map[Point]int{currentPos[i]: 1}
+	}
 
 	for _, line := range lines {
-    if line == "" {
-      continue
-    }
-    direction := line[:1]
-    distance, err := strconv.Atoi(line[2:])
-    check(err)
+		if line == "" {
+			continue
+		}
+		direction := line[:1]
+		distance, err := strconv.Atoi(line[2:])
+		check(err)
 
-    for i := 0; i < distance; i++ {
-      headPos := currentPos[0]
+		for i := 0; i < distance; i++ {
+			headPos := currentPos[0]
 
-      // Move the head.
-      switch direction {
-      case "U":
-        headPos.y += 1
-      case "D":
-        headPos.y -= 1
-      case "R":
-        headPos.x += 1
-      case "L":
-        headPos.x -= 1
-      }
+			// Move the head.
+			switch direction {
+			case "U":
+				headPos.y += 1
+			case "D":
+				headPos.y -= 1
+			case "R":
+				headPos.x += 1
+			case "L":
+				headPos.x -= 1
+			}
 
-      currentPos[0] = headPos
+			currentPos[0] = headPos
 
-      for j := 1; j < 10; j++ {
-        headPos := currentPos[j - 1]
-        tailPos := currentPos[j]
+			for j := 1; j < 10; j++ {
+				headPos := currentPos[j-1]
+				tailPos := currentPos[j]
 
-        dx := headPos.x - tailPos.x
-        dy := headPos.y - tailPos.y
+				dx := headPos.x - tailPos.x
+				dy := headPos.y - tailPos.y
 
-        if absInt(dx) <= 1 && absInt(dy) <= 1 {
-          // Don't move, the head and tail are already touching.
-        } else if absInt(dx) == 0 {
-          // Already in the same column, move only within the column.
-          tailPos.y += sgnInt(dy)
-        } else if absInt(dy) == 0 {
-          // ALready in the same row, move only within the row.
-          tailPos.x += sgnInt(dx)
-        } else {
-          // We need to do a diagonal move.
-          tailPos.x += sgnInt(dx)
-          tailPos.y += sgnInt(dy)
-        }
+				if absInt(dx) <= 1 && absInt(dy) <= 1 {
+					// Don't move, the head and tail are already touching.
+				} else if absInt(dx) == 0 {
+					// Already in the same column, move only within the column.
+					tailPos.y += sgnInt(dy)
+				} else if absInt(dy) == 0 {
+					// ALready in the same row, move only within the row.
+					tailPos.x += sgnInt(dx)
+				} else {
+					// We need to do a diagonal move.
+					tailPos.x += sgnInt(dx)
+					tailPos.y += sgnInt(dy)
+				}
 
-        currentPos[j] = tailPos
-        uniquePos[j][tailPos] = 1
-      }
-    }
+				currentPos[j] = tailPos
+				uniquePos[j][tailPos] = 1
+			}
+		}
 
 		fmt.Printf(
-      "%s %d -> H(%d, %d) T(%d, %d), %d unique positions #1, %d unique positions #9\n",
-      direction, distance,
-      currentPos[0].x, currentPos[0].y,
-      currentPos[1].x, currentPos[1].y,
-      len(uniquePos[1]),
-      len(uniquePos[9]),
-    )
+			"%s %d -> H(%d, %d) T(%d, %d), %d unique positions #1, %d unique positions #9\n",
+			direction, distance,
+			currentPos[0].x, currentPos[0].y,
+			currentPos[1].x, currentPos[1].y,
+			len(uniquePos[1]),
+			len(uniquePos[9]),
+		)
 	}
 }
