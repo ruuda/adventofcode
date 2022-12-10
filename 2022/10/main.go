85996604d66113d9e3ae74cbbf6d283292160294
Ruud van Asseldonk <dev@veniogames.com>
2022-12-10 14:08:27 +0100

    Solve 2022-10 part 1 in Go

--- 2022/10/main.go
+++ 2022/10/main.go
@@ -13,13 +13,17 @@ func check(e error) {
 	}
 }
 
-func main() {
-	data, err := os.ReadFile("example2.txt")
+type Instr struct {
+	latency int
+	deltaX  int
+}
+
+func loadProgram() []Instr {
+	instructions := make([]Instr, 0)
+
+	data, err := os.ReadFile("input.txt")
 	check(err)
 	lines := strings.Split(string(data), "\n")
-
-	var pc int = 0
-	var x int = 1
 
 	for _, line := range lines {
 		if line == "" {
@@ -28,14 +32,44 @@ func main() {
 		parts := strings.Split(line, " ")
 		switch parts[0] {
 		case "noop":
-			pc += 1
-			fmt.Printf("noop\t\tpc=%d x=%d\n", pc, x)
+			instr := Instr{1, 0}
+			instructions = append(instructions, instr)
 		case "addx":
 			delta, err := strconv.Atoi(parts[1])
 			check(err)
-			pc += 2
-			x += delta
-			fmt.Printf("addx %d\t\tpc=%d x=%d\n", delta, pc, x)
+			instr := Instr{2, delta}
+			instructions = append(instructions, instr)
 		}
 	}
+
+	return instructions
+}
+
+func main() {
+	instructions := loadProgram()
+
+	var pc = 0
+	var x = 1
+
+	// Next value of the program counter where we need to sample the X register.
+	var nextSamplePc = 20
+	var totalSignalStrength = 0
+
+	for _, instr := range instructions {
+		pc += instr.latency
+
+		if pc >= nextSamplePc {
+			signalStrength := x * nextSamplePc
+			fmt.Printf("pc=%d\tx=%d\tss=%d\n", nextSamplePc, x, signalStrength)
+			// Instructions say to find the signal strength up to the 220th cycle.
+			if nextSamplePc <= 220 {
+				totalSignalStrength += signalStrength
+			}
+			nextSamplePc += 40
+		}
+
+		x += instr.deltaX
+	}
+
+	fmt.Printf("Part 1, total signal strength: %d\n", totalSignalStrength)
 }
