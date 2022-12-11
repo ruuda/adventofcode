b9b0db4c44909dff039087b50ae72703aaeab329
Ruud van Asseldonk <dev@veniogames.com>
2022-12-11 10:15:02 +0100

    Complete parsing the monkeys

--- 2022/11/main.c
+++ 2022/11/main.c
@@ -19,6 +19,27 @@ typedef struct {
 
 monkey_t monkeys[MAX_MONKEYS];
 
+// Parse a comma-delimited list of integers into the monkey.
+int parse_items(monkey_t* monkey, const char* line) {
+  while (1) {
+    char* result = memchr(line, ',', strlen(line));
+    int* item = monkey->items + monkey->n_items;
+    *item = atoi(line);
+    monkey->n_items++;
+
+    if (monkey->n_items > MAX_ITEMS) {
+      printf("Too many items.\n");
+      return 1;
+    }
+
+    if (result == NULL) return 0;
+
+    // There is more after the comma. atoi skips the space,
+    // so we don't need to skip it here.
+    line = result + 1;
+  }
+}
+
 int main(int argc, const char** argv) {
   assert(argc == 2);
   FILE* f = fopen(argv[1], "r");
@@ -46,8 +67,13 @@ int main(int argc, const char** argv) {
         return 1;
       }
     } else if (MATCHES(line, "  Starting items: ")) {
-      // TODO: Read items.
-      monkey->n_items = 1;
+      size_t offset = strlen("  Starting items: ");
+      monkey->n_items = 0;
+      int result = parse_items(monkey, line + offset);
+      if (result != 0) {
+        printf("Failed to parse monkey %ld.\n", n_monkeys);
+        return 1;
+      }
     } else if (MATCHES(line, "  Operation: new = old ")) {
       size_t offset = strlen("  operation: new = old ");
       monkey->operation = line[offset];
@@ -75,6 +101,9 @@ int main(int argc, const char** argv) {
       i, monkey->n_items, monkey->operation, monkey->rhs, monkey->divisor,
       monkey->to_if_divides, monkey->to_if_not_divides
     );
+    for (int j = 0; j < monkey->n_items; j++) {
+      printf("  item: %d\n", monkey->items[j]);
+    }
   }
 
   free(line);
