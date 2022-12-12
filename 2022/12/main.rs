9ab3e8f05747f1852db59f4272d0ef33b9988b2c
Ruud van Asseldonk <dev@veniogames.com>
2022-12-12 21:55:54 +0100

    Solve 2022-12 part 2

--- 2022/12/main.rs
+++ 2022/12/main.rs
@@ -2,17 +2,15 @@ use std::fs;
 use std::io;
 use std::collections::{HashSet, BinaryHeap};
 
-fn main() -> io::Result<()> {
-    // Parse the field input into a 2d grid. Conveniently the input is ascii,
-    // so we can just treat the letters as 8-bit height values.
-    let raw_field = fs::read_to_string("input.txt")?;
-    let width = raw_field.find('\n').expect("Input must contain newline.");
-    let field: Vec<&[u8]> = raw_field
-        .as_bytes()
-        .chunks(width + 1)
-        .map(|line| &line[..width])
-        .collect();
+/// Return the length of the shortest path from a cell that satisfies the
+/// condition, to `end`.
+pub fn dijkstra_until<F: Fn(u8) -> bool>(
+    field: &[&[u8]],
+    end: (i32, i32),
+    is_start: F,
+) -> i32 {
     let height = field.len();
+    let width = field[0].len();
 
     // The start and end need special treatment to get their elevation.
     let get_z = |(x, y): (i32, i32)| match field[y as usize][x as usize] {
@@ -21,25 +19,12 @@ fn main() -> io::Result<()> {
         z => z,
     };
 
-    // Locate the start and end position in the grid.
-    let mut start = (0, 0);
-    let mut end = (0, 0);
-    for (y, line) in field.iter().enumerate() {
-        for (x, ch) in line.iter().enumerate() {
-            match ch {
-                b'S' => start = (x as i32, y as i32),
-                b'E' => end = (x as i32, y as i32),
-                _ => continue,
-            }
-        }
-    }
-
     // Apply Dijkstra, we keep a set of open nodes ordered by distance from the
     // start, and a set of closed nodes, with the minimum distance to reach
     // them.
     let mut open = BinaryHeap::new();
     let mut closed: HashSet<(i32, i32)> = HashSet::new();
-    open.push((0, start));
+    open.push((0, end));
 
     loop {
         // We store the negative distance, because Rust's BinaryHeap is a
@@ -48,9 +33,8 @@ fn main() -> io::Result<()> {
         let (x, y) = p;
         let zp = get_z(p);
 
-        if p == end {
-            println!("Shortest route to exit: {} steps", -neg_dist);
-            break;
+        if is_start(field[y as usize][x as usize]) {
+            return -neg_dist;
         }
 
         if closed.contains(&p) {
@@ -74,14 +58,44 @@ fn main() -> io::Result<()> {
                 continue;
             }
 
-            if zc > zp + 1 {
-                // We cannot step onto this cell, it's too high.
+            if zp > zc + 1 {
+                // We cannot step from this cell, we are too high.
                 continue;
             }
 
             open.push((neg_dist - 1, c));
         }
     }
+
+}
+
+fn main() -> io::Result<()> {
+    // Parse the field input into a 2d grid. Conveniently the input is ascii,
+    // so we can just treat the letters as 8-bit height values.
+    let raw_field = fs::read_to_string("example.txt")?;
+    let width = raw_field.find('\n').expect("Input must contain newline.");
+    let field: Vec<&[u8]> = raw_field
+        .as_bytes()
+        .chunks(width + 1)
+        .map(|line| &line[..width])
+        .collect();
+
+    // Locate the end position in the grid.
+    let mut end = (0, 0);
+    for (y, line) in field.iter().enumerate() {
+        for (x, ch) in line.iter().enumerate() {
+            match ch {
+                b'E' => end = (x as i32, y as i32),
+                _ => continue,
+            }
+        }
+    }
+
+    let n_steps = dijkstra_until(&field, end, |ch| ch == b'S');
+    println!("Part 1: Shortest route from S to E: {}", n_steps);
+
+    let n_steps = dijkstra_until(&field, end, |ch| ch == b'S' || ch == b'a');
+    println!("Part 2: Shortest route from any a to E: {}", n_steps);
 
     Ok(())
 }
