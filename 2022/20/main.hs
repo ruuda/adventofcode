cba56b5aae366cb077869a5c8ce9d7c250c87207
Ruud van Asseldonk <dev@veniogames.com>
2022-12-20 18:43:56 +0100

    Solve 2022-20 part 2 in Haskell

--- 2022/20/main.hs
+++ 2022/20/main.hs
@@ -30,30 +30,35 @@ rotateNth n xs =
   in
     rotateAt i xs
 
+mix :: [(Int, Int)] -> [(Int, Int)]
+mix xs = foldl' (flip rotateNth) xs [0..((length xs) - 1)]
+
+extractAnswer :: [Int] -> Int
+extractAnswer xs =
+  let
+    indexOf0 = fst $ head $ filter (\(_i, n) -> n == 0) $ zip [0..] xs
+    elem1000 = xs !! ((indexOf0 + 1000) `mod` (length xs))
+    elem2000 = xs !! ((indexOf0 + 2000) `mod` (length xs))
+    elem3000 = xs !! ((indexOf0 + 3000) `mod` (length xs))
+  in
+    elem1000 + elem2000 + elem3000
+
+-- Also in GHC.Utils.Misc, but I can't import from there so let's define it
+-- ourselves.
+nTimes :: Int -> (a -> a) -> (a -> a)
+nTimes 1 f = f
+nTimes n f = f . nTimes (n - 1) f
+
 main :: IO ()
 main = do
   fileLines <- lines <$> readFile "input.txt"
   let
     numbers :: [Int]
     numbers = fmap read fileLines
-    withIndex = zip [0..] numbers
-    indices = [0..((length numbers) - 1)]
-    resultIndex = foldl' (flip rotateNth) withIndex indices
-    result = fmap snd resultIndex
+    withIndex1 = zip [0..] numbers
+    withIndex2 = zip [0..] (fmap (* 811589153) numbers)
+    result1 = fmap snd $ mix withIndex1
+    result2 = fmap snd $ (nTimes 10 mix) withIndex2
 
-    indexOf0 = fst $ head $ filter (\(_i, n) -> n == 0) $ zip [0..] result
-    elem1000 = result !! ((indexOf0 + 1000) `mod` (length numbers))
-    elem2000 = result !! ((indexOf0 + 2000) `mod` (length numbers))
-    elem3000 = result !! ((indexOf0 + 3000) `mod` (length numbers))
-    answer = elem1000 + elem2000 + elem3000
-
-  -- foldM_ (\xs i -> do
-  --     putStrLn $ "Before " <> (show i) <> ": " <> show (fmap snd xs)
-  --     pure $ rotateNth i xs
-  --   ) withIndex indices
-  -- putStrLn $ "After: " <> (show result)
-
-  putStrLn $ show elem1000
-  putStrLn $ show elem2000
-  putStrLn $ show elem3000
-  putStrLn $ show answer
+  putStrLn $ "Part 1: " <> (show $ extractAnswer result1)
+  putStrLn $ "Part 2: " <> (show $ extractAnswer result2)
