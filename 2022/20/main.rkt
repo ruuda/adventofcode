425dbc587968cfa177b1782624d5747944a3fc47
Ruud van Asseldonk <dev@veniogames.com>
2022-12-20 09:09:44 +0100

    Struggle with Racket as well

--- 2022/20/main.rkt
+++ 2022/20/main.rkt
@@ -11,4 +11,26 @@
 
 (define contents (readfile))
 (define numbers (map string->number contents))
-(display numbers)
+(define (zip l1 l2) (map list l1 l2))
+(define workset
+  (list->vector (zip (range (length numbers)) numbers)))
+
+(define (rotate-at vec i)
+  (letrec
+    ((pfx (vector-take vec i))
+     (sfx (vector-drop vec (+ i 1)))
+     (pn     (vector-length pfx))
+     (sn     (vector-length sfx))
+     (elem   (vector-ref vec i))
+     (shift  (car (cdr elem))))
+    (cond
+      ((and (>= shift 0) (<= shift pn))
+        (let-values
+          (((pa pb) (vector-split-at sfx shift)))
+          (vector-append pfx pa #(elem) pb)))
+      (else
+        (vector-append pfx #(elem) sfx)))))
+
+(display workset)
+(newline)
+(display (rotate-at workset 0))
