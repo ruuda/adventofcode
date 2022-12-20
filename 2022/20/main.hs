44f9be46d18599ec48f814570c8f478d1e229cc9
Ruud van Asseldonk <dev@veniogames.com>
2022-12-20 10:08:28 +0100

    Implement the mix function

--- 2022/20/main.hs
+++ 2022/20/main.hs
@@ -2,7 +2,7 @@
 
 module Main where
 
-import Control.Monad (forM_)
+import Control.Monad (forM_, foldM_)
 import Data.List (foldl')
 
 rotateAt :: Int -> [(Int, Int)] -> [(Int, Int)]
@@ -13,8 +13,13 @@ rotateAt i xs =
     (j, n) = xs !! i
     np = length prefix
     ns = length suffix
-    m  = if n >= 0 then n else ns + np + n + 1
-    m' = if n > 0 then n - ns - 1 else np + n
+    -- NB: The puzzle's definition of moving backward is a bit weird, I would
+    -- expect this:
+    -- m  = if n >= 0 then n else ns + np + n + 1
+    -- But when it wraps around, it makes one additional jump, so use -1 instead
+    -- of -1.
+    m  = if n >= 0 then n else ns + np + n
+    m' = if n > 0 then n - ns else np + n
   in
     case () of
       () | n >= 0 && n < ns -> prefix <> (take m suffix) <> [(j, n)] <> (drop m suffix)
@@ -39,5 +44,12 @@ main = do
     indices = [0..((length numbers) - 1)]
     resultIndex = foldl' (flip rotateNth) withIndex indices
     result = fmap snd resultIndex
+
+    -- i = fst $ head $ filter (\(_, (j, _)) -> j == n) $ zip [0..] xs
+
+  -- foldM_ (\xs i -> do
+  --     putStrLn $ "Before " <> (show i) <> ": " <> show (fmap snd xs)
+  --     pure $ rotateNth i xs
+  --   ) withIndex indices
 
   putStrLn $ show result
