cb120ee61747a6fb78cac732868580e69b1962b1
Ruud van Asseldonk <dev@veniogames.com>
2023-12-03 10:24:22 +0100

    Do an initial marking of the map

--- 2023/03/main.d
+++ 2023/03/main.d
@@ -20,8 +20,46 @@ void main() {
     marks ~= marks_line;
   }
 
+  // For the flood fill, we need to maintain a frontier of open nodes. Do a
+  // first pass over the map, and mark all the digit positions adjacent to a
+  // symbol as digit.
+  int[2][] frontier;
+  for (int y = 0; y < data.length; y++) {
+    string line = data[y];
+    for (int x = 0; x < line.length; x++) {
+      switch (line[x]) {
+      case '.':
+        marks[y][x] = '.';
+        break;
+
+      case '0': .. case '9':
+        // Digits themselves are not interesting.
+        break;
+
+      default:
+        // For punctuation that is not '.', the surrounding cells should be
+        // marked.
+        marks[y][x] = '.';
+        for (int dy = -1; dy <= 1; dy++) {
+          int yy = y + dy;
+          if (yy < 0 || yy >= data.length) continue;
+          for (int dx = -1; dx <= 1; dx++) {
+            int xx = x + dx;
+            if (xx < 0 || xx >= line.length) continue;
+            char cell = data[yy][xx];
+            if ('0' <= cell && cell <= '9') {
+              marks[yy][xx] = 'd';
+              frontier ~= [xx, yy];
+            }
+          }
+        }
+      }
+    }
+  }
+
   for (int i = 0; i < data.length; i++) {
+    write(data[i]);
+    write("   ");
     writeln(marks[i]);
-    writeln(data[i]);
   }
 }
