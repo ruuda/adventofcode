3fb3e60ed21e818f4617d0ef2823577bd05affb0
Ruud van Asseldonk <dev@veniogames.com>
2023-12-03 10:40:25 +0100

    Solve day 3 part 1 in D

--- 2023/03/main.d
+++ 2023/03/main.d
@@ -1,9 +1,10 @@
+import std.conv;
 import std.file;
 import std.stdio;
 import std.string;
 
 void main() {
-  File file = File("example.txt", "r");
+  File file = File("input.txt", "r");
 
   // Data holds the input data, marks holds the mark status of each cell.
   string[] data;
@@ -81,10 +82,26 @@ void main() {
     }
   }
 
-  writeln("\nFinal map:");
-  for (int i = 0; i < data.length; i++) {
-    write(data[i]);
-    write("   ");
-    writeln(marks[i]);
+  // Finally, we can walk over the marked map to identify all integers.
+  uint[] part_numbers;
+  for (int y = 0; y < data.length; y++) {
+    string line = data[y];
+    for (int x = 0; x < line.length; x++) {
+      if (marks[y][x] != 'd') continue;
+      int xx = x + 1;
+      for (; xx < line.length; xx++) {
+        if (marks[y][xx] != 'd') break;
+      }
+      string number_str = line[x..xx];
+      uint part_number = to!uint(number_str);
+      part_numbers ~= part_number;
+      x = xx;
+    }
   }
+
+  uint part_number_sum = 0;
+  foreach (part_number; part_numbers) {
+    part_number_sum += part_number;
+  }
+  writeln("Part 1: Sum of part numbers: ", part_number_sum);
 }
