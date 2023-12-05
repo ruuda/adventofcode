80abaaf9c2c7dc9fea47409656703ebfc2bf5fe3
Ruud van Asseldonk <dev@veniogames.com>
2023-12-05 09:04:15 +0100

    Solve day 5 part 1 in Nim

--- 2023/05/main.nim
+++ 2023/05/main.nim
@@ -1,24 +1,43 @@
+import std/sequtils
 import std/strscans
 import std/strutils
-import std/sequtils
 
-for line in "example.txt".lines:
+var seeds: seq[int] = @[]
+var mapped_seeds: seq[int]
+
+for line in "input.txt".lines:
   var dst_start, src_start, range_len: int
   var seeds_str, map_name: string
-  var seeds: seq[int]
 
   if line == "":
     continue
 
   elif scanf(line, "seeds: $+", seeds_str):
-    let seeds = map(split(seeds_str, ' '), parseInt)
+    mapped_seeds = map(split(seeds_str, ' '), parseInt)
 
   elif scanf(line, "$+ map:", map_name):
-    echo "found map: ", line
+    # When a new table starts, we map the currently unmapped seeds as-is.
+    seeds = seeds & mapped_seeds
+    mapped_seeds = @[]
+    echo "Mapped leftover ", seeds
 
   elif scanf(line, "$i $i $i", dst_start, src_start, range_len):
-    echo "Found mapping", dst_start, src_start, range_len
+    var leftover: seq[int] = @[]
+    for i in seeds:
+      if i >= src_start and i < src_start + range_len:
+        let j = i - src_start + dst_start
+        mapped_seeds.add(j)
+        echo "Map ", i, " -> ", j
+      else:
+        leftover.add(i)
+    seeds = leftover
 
   else:
-    echo "bad line", line
     raise newException(Defect, "Unexpected line.")
+
+# Finally, there may be unmapped seeds that we map as-is.
+seeds = seeds & mapped_seeds
+echo "Mapped final ", seeds
+
+let i_min = minIndex(seeds)
+echo "Minimal location: ", seeds[i_min]
