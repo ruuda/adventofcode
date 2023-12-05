79e473f49450425579e62134a1c0507ed8a691ba
Ruud van Asseldonk <dev@veniogames.com>
2023-12-05 08:46:19 +0100

    Try to parse the input in Nim

--- 2023/05/main.nim
+++ 2023/05/main.nim
@@ -1,3 +1,24 @@
+import std/strscans
+import std/strutils
+import std/sequtils
 
-let file = readFile("example.txt")
-echo file
+for line in "example.txt".lines:
+  var dst_start, src_start, range_len: int
+  var seeds_str, map_name: string
+  var seeds: seq[int]
+
+  if line == "":
+    continue
+
+  elif scanf(line, "seeds: $+", seeds_str):
+    let seeds = map(split(seeds_str, ' '), parseInt)
+
+  elif scanf(line, "$+ map:", map_name):
+    echo "found map: ", line
+
+  elif scanf(line, "$i $i $i", dst_start, src_start, range_len):
+    echo "Found mapping", dst_start, src_start, range_len
+
+  else:
+    echo "bad line", line
+    raise newException(Defect, "Unexpected line.")
