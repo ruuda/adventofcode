9fceb416d945093f1ec29c39b238802cb8e6190c
Ruud van Asseldonk <dev@veniogames.com>
2023-12-07 08:52:27 +0100

    Solve day 7 in Nim

--- 2023/07/main.nim
+++ 2023/07/main.nim
@@ -1,11 +1,82 @@
 import std/algorithm
+import std/enumerate
+import std/sequtils
 import std/strscans
+import std/strutils
+import std/sugar
 
-for line in "example.txt".lines:
+let part = 2
+
+func charCount(target: char, s: string): int =
+  filter(s, c => c == target).len
+
+proc charWorth(target: char): int =
+  if part == 1:
+    "23456789TJQKA".find(target)
+  else:
+    "J23456789TQKA".find(target)
+
+func cmpSeq(xs: seq[int], ys: seq[int]): int =
+  for (x, y) in zip(xs, ys):
+    if x == y:
+      continue
+    else:
+      return y - x
+
+# Deconstruct the hand into a sequence of int so the sequence ordering matches
+# the hand order for part 1.
+func deconstructHand1(x: string): seq[int] =
+  sorted(map(x, c => charCount(c, x)), system.cmp[int], Descending)
+
+func deconstructHand2(x: string): seq[int] =
+  var counts = deconstructHand1(x.replace("J", ""))
+
+  if counts.len == 0:
+    # The best we can get is 5 of a kind.
+    return @[5, 5, 5, 5, 5]
+
+  while counts.len < 5:
+    for i in countup(0, counts[0] - 1):
+      counts[i] += 1
+    counts.insert(counts[0], 0)
+
+  counts
+
+proc cmpHand(x, y: string): int =
+  let cx = if part == 1: deconstructHand1(x) else: deconstructHand2(x)
+  let cy = if part == 1: deconstructHand1(y) else: deconstructHand2(y)
+  let d = cmpSeq(cx, cy)
+  if d == 0:
+    cmpSeq(map(x, charWorth), map(y, charWorth))
+  else:
+    d
+
+type Hand = object
+  hand: string
+  bid: int
+
+func initHand(hand: string, bid: int): Hand =
+  result.hand = hand
+  result.bid = bid
+
+var hands: seq[Hand] = @[]
+
+for line in "input.txt".lines:
   var hand: string
   var bid: int
 
   if not scanf(line, "$+ $i", hand, bid):
     continue
 
-  echo hand, " ", bid
+  hands.add(initHand(hand, bid))
+  echo hand, " -> ", deconstructHand2(hand)
+
+var winnings_total = 0
+
+for (i, hand) in enumerate(sorted(hands, (h1, h2) => cmpHand(h1.hand, h2.hand))):
+  let rank = hands.len - i
+  let winnings = hand.bid * rank
+  echo rank, " ", hand
+  winnings_total += winnings
+
+echo winnings_total
--- README.md
+++ README.md
@@ -10,26 +10,26 @@ solution must be written in either:
  * A language that I haven’t used before in this repository.
  * A language in the bottom 50% used in this repository, by lines of code.
 
-Languages used as of 2023 day 4, as computed by `languages.sh`:
+Languages used as of 2023 day 7, as computed by `languages.sh`:
 
 ```
 Language   %Used   %Cumulative   Files   Allowed Next
-Pony        15.1          15.1       4   No
-Haskell     13.0          28.0       7   No
-Python      10.8          38.9      10   No
-C           10.4          49.3       2   No
-Rust        10.1          59.4       5   No
-Go           9.1          68.5       3   Yes
-AWK          7.6          76.0      10   Yes
-Nix          6.3          82.3       5   Yes
-RCL          3.7          86.0       3   Yes
-D            3.3          89.3       1   Yes
-Nim          2.7          92.0       2   Yes
-SQL          2.4          94.4       1   Yes
-Zig          2.2          96.6       1   Yes
-Unison       1.1          97.7       1   Yes
-Racket       0.9          98.6       1   Yes
-Shell        0.9          99.4       1   Yes
+Pony        14.8          14.8       4   No
+Haskell     12.7          27.6       7   No
+Python      10.6          38.2      10   No
+C           10.2          48.5       2   No
+Rust         9.9          58.4       5   No
+Go           9.0          67.4       3   Yes
+AWK          7.4          74.8      10   Yes
+Nix          6.2          81.0       5   Yes
+Nim          4.2          85.3       3   Yes
+RCL          3.6          88.9       3   Yes
+D            3.3          92.1       1   Yes
+SQL          2.4          94.5       1   Yes
+Zig          2.2          96.7       1   Yes
+Unison       1.1          97.8       1   Yes
+Racket       0.8          98.6       1   Yes
+Shell        0.8          99.4       1   Yes
 Scheme       0.6         100.0       1   Yes
 ```
 Some languages that I still want to try:
