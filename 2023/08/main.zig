42655f4e78f981bfe9bc1a43063215df1e8b91e5
Ruud van Asseldonk <dev@veniogames.com>
2023-12-08 09:12:43 +0100

    Brute-force day 8 part 2 in Zig
    I suspect the proper way would be to compute the periods of all of these
    cycles and take their least common multiple, but the problem is that a
    cycle may end up at a Z multiple times, it does not have a single
    period.

--- 2023/08/main.zig
+++ 2023/08/main.zig
@@ -12,12 +12,12 @@ const Input = struct {
     nodes: std.AutoHashMap(Loc, Node),
 };
 
-fn read_input() !Input {
+fn read_input(fname: []const u8) !Input {
     const stdout = std.io.getStdOut().writer();
     const allocator = std.heap.page_allocator;
     var result = std.AutoHashMap(Loc, Node).init(allocator);
 
-    const file = try std.fs.cwd().openFile("input.txt", .{});
+    const file = try std.fs.cwd().openFile(fname, .{});
     defer file.close();
 
     var buf_reader = std.io.bufferedReader(file.reader());
@@ -42,9 +42,9 @@ fn read_input() !Input {
     return Input{ .nav = nav_owned, .nodes = result };
 }
 
-pub fn main() !void {
+pub fn part1(fname: []const u8) !void {
     const stdout = std.io.getStdOut().writer();
-    const input = try read_input();
+    const input = try read_input(fname);
 
     var loc: [3]u8 = "AAA".*;
     var cursor: usize = 0;
@@ -61,4 +61,54 @@ pub fn main() !void {
         cursor = (cursor + 1) % input.nav.len;
         try stdout.print("Step {d} to {s}.\n", .{ steps, loc });
     }
+}
+
+pub fn part2(fname: []const u8) !void {
+    const stdout = std.io.getStdOut().writer();
+    const input = try read_input(fname);
+
+    const allocator = std.heap.page_allocator;
+    var locs = std.ArrayList([3]u8).init(allocator);
+
+    var keys = input.nodes.keyIterator();
+    while (keys.next()) |k| {
+        if (k[2] == 'A') {
+            try locs.append(k.*);
+        }
+    }
+
+    var cursor: usize = 0;
+    var steps: usize = 0;
+
+    while (true) {
+        var at_end = true;
+        for (locs.items) |loc| {
+            if (loc[2] != 'Z') {
+                at_end = false;
+                break;
+            }
+        }
+
+        if (at_end) {
+            break;
+        }
+
+        steps = steps + 1;
+        for (locs.items) |*loc| {
+            const node = input.nodes.get(loc.*).?;
+            if (input.nav[cursor] == 'L') {
+                loc.* = node.left;
+            } else {
+                loc.* = node.right;
+            }
+            // try stdout.print("Step {d} to {s}.\n", .{ steps, loc });
+        }
+        cursor = (cursor + 1) % input.nav.len;
+    }
+    try stdout.print("Took {d} steps.\n", .{steps});
+}
+
+pub fn main() !void {
+    // try part1("example1.txt");
+    try part2("input.txt");
 }
