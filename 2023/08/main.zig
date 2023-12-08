cfbacc0fee6f994cc0741c69b23f63a04d62b4a6
Ruud van Asseldonk <dev@veniogames.com>
2023-12-08 21:42:20 +0100

    Speed up stepping through the cycles

--- 2023/08/main.zig
+++ 2023/08/main.zig
@@ -67,15 +67,24 @@ pub fn part2(fname: []const u8) !void {
     const stdout = std.io.getStdOut().writer();
     const input = try read_input(fname);
     const allocator = std.heap.page_allocator;
+    var start_indices = std.ArrayList(usize).init(allocator);
+    var all_jumps = std.ArrayList(std.ArrayList(usize)).init(allocator);
 
+    // Loop over all the keys that are starting positions.
     var keys = input.nodes.keyIterator();
     while (keys.next()) |k| {
         if (k[2] != 'A') continue;
 
+        // For this key, we are going to build the path of locations that it
+        // visits. Eventually this path repeats, and as soon as we have a cycle,
+        // we stop looking further.
+        var start_index: usize = 0;
         var path = std.ArrayList([3]u8).init(allocator);
+        var jumps = std.ArrayList(usize).init(allocator);
         var cursor: usize = 0;
         var loc: [3]u8 = k.*;
         try path.append(loc);
+        try jumps.append(0);
 
         while (true) {
             const node = input.nodes.get(loc).?;
@@ -89,17 +98,97 @@ pub fn part2(fname: []const u8) !void {
             var found_cycle = false;
             for (path.items, 0..) |prev, i| {
                 if (std.mem.eql(u8, &loc, &prev) and cursor == i % input.nav.len) {
+                    start_index = i;
                     found_cycle = true;
                     break;
                 }
             }
 
-            if (found_cycle) break;
+            if (found_cycle) {
+                try start_indices.append(start_index);
+                break;
+            }
 
             try path.append(loc);
+            try jumps.append(0);
         }
 
+        // Now that we have the cycle, identify all end node locations in it,
+        // and how far ahead we need to jump to get there.
+        for (0..path.items.len) |i| {
+            if (i > 0) {
+                // If for the previous place we needed to jump j ahead to an end
+                // node, then now we need to jump j-1 ahead.
+                const prev = jumps.items[i - 1];
+                if (prev > 1) {
+                    jumps.items[i] = prev - 1;
+                    continue;
+                }
+            }
+
+            for (1..path.items.len) |j| {
+                var jmod = i + j;
+                if (jmod >= path.items.len) jmod = jmod - path.items.len + start_index;
+                if (path.items[j][2] != 'Z') continue;
+                jumps.items[i] = @truncate(j);
+                break;
+            }
+        }
+
+        try all_jumps.append(jumps);
         try stdout.print("Key {s} has a period of {d}.\n", .{ k, path.items.len });
+    }
+
+    var steps_since_print: usize = 0;
+    var steps: usize = 0;
+    var cursors = std.ArrayList(usize).init(allocator);
+    for (all_jumps.items) |_| {
+        try cursors.append(0);
+    }
+
+    while (true) {
+        if (steps_since_print > 1000 * 1000) {
+            steps_since_print = 0;
+            try stdout.print("Step {d}.\n", .{steps});
+        }
+        // If the jump just before the cursor is 1 for all cursors, then the
+        // cursors are at an end position.
+        if (steps > 0) {
+            var is_end = true;
+            for (cursors.items, all_jumps.items) |cursor, jumps| {
+                if (jumps.items[cursor - 1] > 1) {
+                    is_end = false;
+                    break;
+                }
+            }
+            if (is_end) {
+                try stdout.print("Took {d} steps.\n", .{steps});
+                break;
+            }
+        }
+
+        // Determine how far to jump ahead. Take the maximum jump among all the
+        // cursors. If all the cursors want to jump by the same amount, then
+        // it's the jump to the end.
+        var jump = all_jumps.items[0].items[cursors.items[0]];
+        for (cursors.items, all_jumps.items, 0..) |cursor, jumps, i| {
+            const j = jumps.items[cursor];
+            // try stdout.print("  {d}: {d} +{d}.\n", .{ i, cursor, j });
+            if (j == 0) unreachable;
+            if (i == 0) continue;
+            if (j == jump) continue;
+            if (j > jump) jump = j;
+        }
+
+        steps += jump;
+        steps_since_print += jump;
+
+        for (cursors.items, start_indices.items, all_jumps.items) |*cursor, start_index, jumps| {
+            cursor.* = cursor.* + jump;
+            while (cursor.* >= jumps.items.len) {
+                cursor.* -= jumps.items.len - start_index;
+            }
+        }
     }
 }
 
