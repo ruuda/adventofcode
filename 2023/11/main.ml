826052f53d16cb8ac9488aef6806ebbfd89df579
Ruud van Asseldonk <dev@veniogames.com>
2023-12-11 09:21:12 +0100

    Identify empty rows/columns in OCaml

--- 2023/11/main.ml
+++ 2023/11/main.ml
@@ -1,6 +1,8 @@
 (* Run with `ocaml main.ml` or `ocamlopt -o main main.ml && main`. *)
 let file = "example.txt"
 
+module IntSet = Set.Make(Int);;
+
 let locate_galaxies: string list -> (int * int) list = fun rows ->
   let fold_row = fun y acc row ->
     String.fold_left
@@ -14,9 +16,34 @@ let locate_galaxies: string list -> (int * int) list = fun rows ->
     (0, []) rows in
   res
 
+let locate_empty_rows: string list -> int list = fun rows ->
+  let (_, res) = List.fold_left
+    (fun (y, res) row ->
+      if String.contains row '#'
+      then (y + 1, res)
+      else (y + 1, y :: res)
+    )
+    (0, []) rows in
+  res
+
+let locate_empty_columns: string list -> int list = fun rows ->
+  let empties = fun row ->
+    let (_, res) = String.fold_left
+      (fun (x, res) ch -> match ch with
+        | '.' -> (x + 1, IntSet.add x res)
+        | '#' -> (x + 1, res)
+        | _ -> failwith "Unexpected character on the map."
+      ) (0, IntSet.empty) row in
+    res in
+  let sets = List.map empties rows in
+  let res = List.fold_left IntSet.inter (List.hd sets) sets in
+  List.rev (IntSet.elements res)
+
 let () =
   let map = In_channel.with_open_text "example.txt" In_channel.input_lines in
   List.iter print_endline map;
   let galaxies = locate_galaxies map in
   List.iter (fun (x, y) -> Printf.printf "%i %i\n" x y) galaxies;
+  List.iter (Printf.printf "COL %i\n") (locate_empty_columns map);
+  List.iter (Printf.printf "ROW %i\n") (locate_empty_rows map);
   flush stdout
