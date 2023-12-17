a62595229eebfbf3f3ed2e9f0397e8bf6fb7418f
Ruud van Asseldonk <dev@veniogames.com>
2023-12-17 10:02:41 +0100

    Solve day 17 part 1 in Clojure
    I got the wrong answer at first, then added the route to the nodes, and
    now I am getting the right answer? Huh, weird ... Also I was getting
    stack overflow when I added the route, converting it to loop/recur then
    got me the right answer. I am still a bit confused.

--- 2023/17/src/main.clj
+++ 2023/17/src/main.clj
@@ -6,15 +6,15 @@
 
 (def field
   "The input map/field that we are pathfinding on."
-  (str/split-lines (slurp "example.txt")))
+  (str/split-lines (slurp "input.txt")))
 
 (def field-w (count (get field 0)))
 (def field-h (count field))
 
 (defn at-exit? [node]
   "Test whether this node is the exit."
-  (and (= (get node :x) (- field-w 1)
-       (= (get node :y) (- field-h 1)))))
+  (and (= (get node :x) (dec field-w))
+       (= (get node :y) (dec field-h))))
 
 (defn in-bounds? [node]
   "Test whether this node lies on the map."
@@ -23,14 +23,17 @@
        (< (get node :x) field-w)
        (< (get node :y) field-h)))
 
-(defn heat-loss [x y]
-  (let [ch (get (get field y) x)] (Character/digit ch 10)))
+(defn heat-loss [node]
+  (let [x (get node :x)
+        y (get node :y)
+        ch (get (get field y) x)]
+    (Character/digit ch 10)))
 
 (def start-nodes
   "We start in the top-left, moving east or south, with 3 steps before a forced turn."
   (priority-map/priority-map
-    {:x 0 :y 0 :dir :east :budget 3} 0
-    {:x 0 :y 0 :dir :south :budget 3} 0))
+    {:x 0 :y 0 :dir :east :budget 3 :route []} 0
+    {:x 0 :y 0 :dir :south :budget 3 :route []} 0))
 
 (defn turns [dir]
   "Return the two directions in which we can turn from `dir`."
@@ -44,40 +47,43 @@
   "Adjust :x and :y to take a step in the given direction."
   (let [x (get node :x) y (get node :y)]
     {:x (cond (= :east dir) (inc x) (= :west dir) (dec x) :else x)
-     :y (cond (= :north dir) (dec y) (= :south dir) (inc x) :else y)
+     :y (cond (= :north dir) (dec y) (= :south dir) (inc y) :else y)
      :dir dir
      ; If we took a step in a different direction than we were going, the budget
      ; resets to 3 and we use 1, so we have 2 steps remaining. If we stepped in
      ; the same direction, just decrement the budget.
-     :budget (if (= dir (get node :dir)) (dec (get node :budget)) 2)}))
+     :budget (if (= dir (get node :dir)) (dec (get node :budget)) 2)
+     :route (cons {:x x :y y} (get node :route))}))
 
 (defn steps [node]
   "Return all steps that we can take from the current node."
   (let
     [dir (get node :dir)
-     new-dirs [conj (turns dir) dir]
+     new-dirs (conj (turns dir) dir)
      new-nodes (map (fn [d] (step d node)) new-dirs)]
-    (filter (fn [n] (>= (get n :budget) 0)) new-nodes)))
+    (filter
+      (fn [n] (and
+                (>= (get n :budget) 0)
+                (in-bounds? n)))
+      new-nodes)))
 
-(defn find-route-rec [open closed]
-  (let [[node cost] (peek open)
-        new-open (pop open)
-        new-closed (conj closed node)]
-    (cond
-      ; If we are at the exit, the final result is the cost it took to reach it.
-      (at-exit? node) cost
-      ; If the node is out of bounds, just drop it and continue.
-      (not (in-bounds? node)) (find-route-rec new-open closed)
-      ; Same if we visited the node before.
-      (contains? closed node) (find-route-rec new-open closed)
-      :else
-        (let [new-nodes (steps node)
-              add-opens (map (fn [n] [n (inc cost)]) new-nodes)
-              new-open' (apply conj new-open add-opens)]
-          (find-route-rec new-open' new-closed)))))
+(def best-route-cost
+  (loop
+    [open start-nodes
+     closed #{}]
+    (let [[node cost] (peek open)
+          new-open (pop open)
+          new-closed (conj closed (dissoc node :route))]
+      (cond
+        ; If we are at the exit, the final result is the cost it took to reach it.
+        (at-exit? node) cost
+        ; Skip nodes that we visited before.
+        (contains? closed (dissoc node :route)) (recur new-open closed)
+        :else
+          (let [new-nodes (steps node)
+                add-opens (map (fn [n] [n (+ cost (heat-loss n))]) new-nodes)
+                new-open' (apply conj new-open add-opens)]
+            (recur new-open' new-closed))))))
 
 (defn run [opts]
-  (println "The input is:")
-  (println field)
-  (println "The field size is:" field-w field-h)
-  (println "At 5,5 we have:" (heat-loss 5 5)))
+  (println "Part 1:" best-route-cost))
