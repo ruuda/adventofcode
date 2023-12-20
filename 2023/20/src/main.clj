92c9f381247e92bc7730f10da0e3cffc15a31661
Ruud van Asseldonk <dev@veniogames.com>
2023-12-20 12:21:48 +0100

    Solve 20.1 in Clojure
    But part 2 is tricky again. I have some idea of what direction to search
    in, but I'm not going to attempt that right now.

--- 2023/20/src/main.clj
+++ 2023/20/src/main.clj
@@ -6,7 +6,7 @@
 
 (def wires
   "The wires that connect the different components."
-  (str/split-lines (slurp "example2.txt")))
+  (str/split-lines (slurp "input.txt")))
 
 (defn parse-module [line]
   "Parse a module into [kind, name, outputs]."
@@ -23,6 +23,18 @@
     (fn [acc module] (assoc acc (get module :name) module))
     {}
     (map parse-module wires)))
+
+(def initial-states
+  (reduce
+    (fn [states module]
+      (let
+        [src (get module :name)]
+        (reduce
+         (fn [st dst] (assoc st dst (assoc (get st dst {}) src false)))
+         states
+         (get module :outputs))))
+    {}
+    (vals modules)))
 
 (defn sim-flip [state outputs sender sig]
   "Send a signal to the flipflop, return [new-state pulses]."
@@ -41,9 +53,9 @@
   "Copy the signal to all outputs, return [new-state pulses]."
   [state (map (fn [o] [o sig]) outputs)])
 
-(def defaults
+(defn default-state [mod-name]
   {"%" false
-   "&" {}
+   "&" (get initial-states mod-name)
    "i" false})
 
 (def sims
@@ -52,36 +64,57 @@
    "i" sim-broadcast})
 
 (defn sim [state pending]
-  (if (empty? pending)
-    state
-    (let
-      [[src dst sig] (first pending)
-       dst-mod (get modules dst {:kind "i" :outputs []})
-       kind (get dst-mod :kind)
-       outputs (get dst-mod :outputs)
-       sim-module (get sims kind)
-       default (get defaults kind)
-       dst-state (get state dst default)
-       [dst-new-state pulses] (sim-module dst-state outputs src sig)
-       ; Prepend the sender to each of the pulses. The sender of the new
-       ; pulses is the destination of the pulse we're currently processing.
-       addr-pulses (map (fn [[p-dst p-sig]] [dst p-dst p-sig]) pulses)
-       new-state (assoc state dst dst-new-state)]
-      (println src "--[" sig "]-->" dst "::" pulses)
-      ; (println "  " dst "state:" dst-state "->" dst-new-state)
-      ; (println "   all states:" new-state)
-      [new-state (into (vec (drop 1 pending)) addr-pulses)])))
+  "Simulate a single pending signal, return the next state and pending signals."
+  (let
+    [[src dst sig] (first pending)
+     dst-mod (get modules dst {:kind "i" :outputs []})
+     kind (get dst-mod :kind)
+     outputs (get dst-mod :outputs)
+     sim-module (get sims kind)
+     default (get (default-state dst) kind)
+     dst-state (get state dst default)
+     [dst-new-state pulses] (sim-module dst-state outputs src sig)
+     ; Prepend the sender to each of the pulses. The sender of the new
+     ; pulses is the destination of the pulse we're currently processing.
+     addr-pulses (map (fn [[p-dst p-sig]] [dst p-dst p-sig]) pulses)
+     new-state (assoc state dst dst-new-state)]
+    (println src "--[" sig "]-->" dst "::" pulses)
+    ; (println "  " dst "state:" dst-state "->" dst-new-state)
+    ; (println "   all states:" new-state)
+    [new-state (into (vec (drop 1 pending)) addr-pulses)]))
 
-(def sim-all
+(defn sim-press-once [state pulse-count]
+  "Simulate one button press until pulses quiesce, return [new-state pulse-count]."
   (loop
-    [state {}
-     pending [["button" "broadcaster" false]]]
+    [state state
+     pending [["button" "broadcaster" false]]
+     pulse-count pulse-count]
     (if (empty? pending)
-      (println "Simulation is done")
+      [state pulse-count]
       (let
-        [[new-state new-pending] (sim state pending)]
-        (recur new-state new-pending)))))
+        [[_src _dst sig] (first pending)
+         [new-state new-pending] (sim state pending)
+         new-pulse-count (assoc pulse-count sig (inc (get pulse-count sig)))]
+        (recur new-state new-pending new-pulse-count)))))
+
+(def sim-press-1000
+  (loop
+    [n-left 1000
+     state {}
+     pulse-count {false 0 true 0}]
+    (if (= 0 n-left)
+      pulse-count
+      (let
+        [[new-state new-pulse-count] (sim-press-once state pulse-count)]
+        (println "Iteration" (- 1001 n-left))
+        (recur (dec n-left) new-state new-pulse-count)))))
 
 (defn run [opts]
   (println "Modules" modules)
-  (println "Part 1:" "TODO"))
+  (println "Initial states" initial-states)
+  (println "Part 1:" (apply * (vals sim-press-1000))))
+
+; For part 2, probably this input is simulating an n-bit adder or something and
+; it will take something like 2^{large-ish} button presses to count up and send
+; a low pulse to `rx`. Instead of going forward and seeing when it happens,
+; probably we need to work backwards and do it symbolically again.
