c449f24417904eb78446af3d8917870d1f10bca5
Ruud van Asseldonk <dev@veniogames.com>
2023-12-20 09:11:50 +0100

    Read input, define simulation, in Clojure
    I have defined the simulation, but not tested this yet, it's probably
    full of bugs.

---
+++ 2023/20/src/main.clj
@@ -0,0 +1,67 @@
+;; Execute with `clj -X main/run` from the parent directory.
+(ns main
+  (:require [clojure.core.reducers :as r])
+  (:require [clojure.java.io :as io])
+  (:require [clojure.string :as str]))
+
+(def wires
+  "The wires that connect the different components."
+  (str/split-lines (slurp "example1.txt")))
+
+(defn parse-module [line]
+  "Parse a module into [kind, name, outputs]."
+  (let [[inputs outputs] (str/split line #" -> ")]
+    { :kind (cond
+               (str/starts-with? line "&") "&"
+               (str/starts-with? line "%") "%"
+               :else "i")
+      :name (str/replace (str/replace inputs "&" "") "%" "")
+      :outputs (str/split outputs #", ") }))
+
+(def modules
+  (reduce
+    (fn [acc module] (conj acc {(get module :name) module}))
+    {}
+    (map parse-module wires)))
+
+(defn sim-flip [state outputs sender sig]
+  "Send a signal to the flipflop, return [new-state pulses]."
+  (if sig
+    [(not state) (map (fn [o] [o (not state)]) outputs)]
+    [state []]))
+
+(defn sim-conj [state outputs sender sig]
+  "Send a signal to the conjunction, return [new-state pulses]."
+  (let
+    [new-state (conj state {sender sig})
+     all-active (and (vals new-state))]
+    [new-state [map (fn [o] [o (not all-active)]) outputs]]))
+
+(defn sim-broadcast [state outputs sender sig]
+  "Copy the signal to all outputs, return [new-state pulses]."
+  [state [map (fn [o] [o sig]) outputs]])
+
+(defn sim [state pending]
+  (if (empty? pending)
+    state
+    (let [[src dst sig] (peek pending)
+          pending' (pop pending)
+          kind (get dst :kind)
+          outputs (get dst :outputs)
+          defaults {"%" false "&" {}}
+          sims {"%" sim-flip
+                "&" sim-conj
+                "i" sim-broadcast}
+          sim (get sims kind)
+          default (get defaults kind)
+          dst-state (get dst state default)
+          [dst-new-state pulses] (sim dst-state outputs src sig)
+          ; Prepend the sender to each of the pulses. The sender of the new
+          ; pulses is the destination of the pulse we're currently processing.
+          addr-pulses (map (fn [[p-dst p-sig]] [dst p-dst p-sig]) pulses)
+          new-state (assoc state dst dst-new-state)]
+    [new-state (concat (pop pending) addr-pulses)])))
+
+(defn run [opts]
+  (println "Modules" modules)
+  (println "Part 1:" "TODO"))
