1ba97a0b3c163c9dcc08c65d6137b5dde4799cb4
Ruud van Asseldonk <dev@veniogames.com>
2023-12-21 08:52:17 +0100

    Read file in C#

--- 2023/21/Program.cs
+++ 2023/21/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+
+void Main()
+{
+  var lines = File.ReadLines("example.txt");
+  foreach (var line in lines)
+  {
+    Console.WriteLine(String.Format("Hello {0}", line));
+  }
+}
+
+Main();
