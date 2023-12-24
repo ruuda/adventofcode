0bfe65392fb75cac9652607c9d3b28aba60f3d28
Ruud van Asseldonk <dev@veniogames.com>
2023-12-24 12:03:23 +0100

    Compute intersections in Go
    We have NaNs, some lines are parallel ...

--- 2023/24/main.go
+++ 2023/24/main.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"strconv"
 	"strings"
+  "math"
 )
 
 func check(e error) {
@@ -13,9 +14,55 @@ func check(e error) {
 	}
 }
 
+type Vec2 struct {
+  x float64
+  y float64
+}
+
+func (v Vec2) Norm() float64 {
+  return math.Sqrt(v.x * v.x + v.y * v.y)
+}
+
+func (v Vec2) Normed() Vec2 {
+  norm := v.Norm()
+  return Vec2 { v.x / norm, v.y / norm }
+}
+
+func (v Vec2) Tangent() Vec2 {
+  return Vec2 { -v.y, v.x }
+}
+
+func (v Vec2) Add(w Vec2) Vec2 {
+  return Vec2 { v.x + w.x, v.y + w.y}
+}
+
+func (v Vec2) Sub(w Vec2) Vec2 {
+  return Vec2 { v.x - w.x, v.y - w.y}
+}
+
+func (v Vec2) Mul(t float64) Vec2 {
+  return Vec2 { v.x * t, v.y * t }
+}
+
+func (v Vec2) Dot(w Vec2) float64 {
+  return v.x * w.x + v.y * w.y
+}
+
 type Hailstone struct {
 	p [3]int
   v [3]int
+}
+
+func (h Hailstone) Pos2() Vec2 {
+  x := float64(h.p[0])
+  y := float64(h.p[1])
+  return Vec2 { x, y }
+}
+
+func (h Hailstone) Vel2() Vec2 {
+  x := float64(h.v[0])
+  y := float64(h.v[1])
+  return Vec2 { x, y }
 }
 
 func loadInput(fname string) []Hailstone {
@@ -49,8 +96,47 @@ func loadInput(fname string) []Hailstone {
 	return hailstones
 }
 
+// Return whether two hailstone trajectories intersect as defined in part 1.
+func intersect2D(a Hailstone, b Hailstone) bool {
+  va := a.Vel2().Normed()
+  vb := b.Vel2().Normed()
+  pa := a.Pos2()
+  pb := b.Pos2()
+  // Translate everything so pa is the origin.
+  // We project the point (pb + tb * vb) onto the line defined by va.
+  // On the line, we have:
+  //    (pb + tb * vb) dot (va.Tangent) = 0
+  // => (pb dot va.Tangent) + ((tb * vb) dot va.Tangent) = 0
+  // => (pb dot va.Tangent) = -tb * (vb dot va.Tangent)
+  // => tb = -(pb dot va.Tangent) / (vb dot va.Tangent)
+  tb := -(pb.Sub(pa)).Dot(va.Tangent()) / (vb.Dot(va.Tangent()))
+  ip := pa.Add(pb).Add(vb.Mul(tb))
+	fmt.Printf("Got %f,%f hailstones\n", ip.x, ip.y)
+
+  if (ip.x < 200000000000000.0) {
+    return false;
+  }
+  if (ip.y < 200000000000000.0) {
+    return false;
+  }
+  if (ip.x > 400000000000000.0) {
+    return false;
+  }
+  if (ip.y > 400000000000000.0) {
+    return false;
+  }
+  return true;
+}
+
 func main() {
   hailstones := loadInput("example.txt")
 	fmt.Printf("Got %d hailstones\n", len(hailstones))
+  for i, a := range hailstones {
+    for j, b := range hailstones {
+      if (j < i) {
+        continue
+      }
+      intersect2D(a, b)
+    }
+  }
 }
-
