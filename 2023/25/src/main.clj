cbadef86ac5907d177f971c86e1e53abc06ee129
Ruud van Asseldonk <dev@veniogames.com>
2023-12-25 12:18:18 +0100

    Implement graph contraction in Clojure

--- 2023/25/src/main.clj
+++ 2023/25/src/main.clj
@@ -7,24 +7,46 @@
   "Lines of the form 'node: node node node'."
   (str/split-lines (slurp "example.txt")))
 
-(defn lex-swap [n0 n1]
-  "Return n0 and n1 in lexicographic order."
-  (if (<= 0 (compare n0 n1)) [n0 n1] [n1 n0]))
-
 (defn parse-append-edge-spec [seed-edges edge-spec]
+  "Parse the input into a symmetric map of node: neighbors."
   (let
     [[n0 others] (str/split edge-spec #": ")
      nodes (str/split others #" ")]
     (defn append-n0 [edges n1]
       (let
-        [[m0 m1] (lex-swap n0 n1)
-         m0-old (get edges m0 #{})
-         m0-new (conj m0-old m1)]
-        (assoc edges m0 m0-new)))
+        [n0-old (get edges n0 {})
+         n1-old (get edges n1 {})
+         n0-new (assoc n0-old n1 1)
+         n1-new (assoc n1-old n0 1)]
+        (assoc edges n0 n0-new n1 n1-new)))
     (reduce append-n0 seed-edges nodes)))
 
 (def initial-graph
   (reduce parse-append-edge-spec {} edge-specs))
 
+; We implement Karger's algorithm, which contracts two nodes in the graph until
+; we have only two nodes left, which have some probability of being a minimum
+; cut of the graph. The exercise already tells us the size of the minimum cut,
+; so we can just try until we find a cut of the right size, assuming the
+; exercise chose the graph such that it has a unique minimum cut.
+
+(defn contract [graph n0 n1]
+  "Return a copy of the graph, with nodes n0 and n1 contracted as n2."
+  (let
+    [out-n0 (get graph n0)
+     out-n1 (get graph n1)
+     out-n2 (dissoc (merge-with + out-n0 out-n1) n0 n1)
+     n2 (str n0 n1)
+     ; At this point we have a map with the old two nodes removed, and replaced
+     ; with the new nodes ... but the edges the other way still point at the old
+     ; nodes, so we also need to visit the old outgoing edges and update them.
+     graph-0 (assoc (dissoc graph n0 n1) n2 out-n2)
+     contract-n01 (fn [edges] (assoc (dissoc edges n0 n1) n2 (+ (get edges n0 0) (get edges n1 0))))]
+    (reduce
+      (fn [g n-out] (update g n-out contract-n01))
+      graph-0
+      (keys out-n2))))
+
 (defn run [opts]
-  (println "Graph" initial-graph))
+  (println "Graph" initial-graph)
+  (println "hfx-xhk" (contract initial-graph "hfx" "xhk")))
