aa530bdc6df2c7bddae955ee3d8d7b06faa02102
Ruud van Asseldonk <dev@veniogames.com>
2024-12-08 10:47:25 +0100

    Add Zig boilerplate for 2024-08a
    Copied from last year, it still works. Also, blub ik ben een goudvis,
    I don't remember anything about how Zig works ...

---
+++ 2024/08/main.zig
@@ -0,0 +1,29 @@
+const std = @import("std");
+
+fn read_input(fname: []const u8) ![][]u8 {
+    const allocator = std.heap.page_allocator;
+    var board = std.ArrayList([]u8).init(allocator);
+
+    const file = try std.fs.cwd().openFile(fname, .{});
+    defer file.close();
+
+    var buf_reader = std.io.bufferedReader(file.reader());
+    const in_stream = buf_reader.reader();
+    var buf: [2048]u8 = undefined;
+
+    while (try in_stream.readUntilDelimiterOrEof(&buf, '\n')) |line| {
+        try board.append(try allocator.dupe(u8, line));
+    }
+
+    return board.items;
+}
+
+pub fn part1(fname: []const u8) !void {
+    const stdout = std.io.getStdOut().writer();
+    const board = try read_input(fname);
+    try stdout.print("Hello Zig, we meet once again. {}", .{board.len});
+}
+
+pub fn main() !void {
+    try part1("example.txt");
+}
