9046f99bb3f9efd4f357a9f5ca29321f92393776
Ruud van Asseldonk <dev@veniogames.com>
2024-12-09 11:11:27 +0100

    Defragment the disk in OCaml

--- 2024/09/main.ml
+++ 2024/09/main.ml
@@ -18,10 +18,38 @@ let rec parse_disk : int -> string -> int list = fun fid disk_map ->
   else
     []
 
+let rec defragment : int list -> int list = fun sectors ->
+  if List.length sectors < 2 then
+    sectors
+  else
+    (* Very inefficient, I know, but it will do. *)
+    let h = List.hd sectors in
+    let rev_sectors = List.rev sectors in
+    let t = List.hd rev_sectors in
+    if t == -1 then
+      (* If there is a gap at the end, just drop it. *)
+      defragment (List.rev (List.tl rev_sectors))
+    else if h == -1 then
+      (* If we have a gap, move the last sector in there. *)
+      let tail = List.rev (List.tl (List.rev (List.tl sectors))) in
+      List.cons t (defragment tail)
+    else
+      (* If we have no dap, leave in place, defragment the remainder. *)
+      List.cons h (defragment (List.tl sectors))
+
+let checksum : int list -> int = fun sectors ->
+  List.fold_left (fun acc x -> acc + x) 0
+  (List.mapi (fun i fid -> i * fid) sectors)
+
 let () =
   (* There is only a single line, but we can cut off the trailing newline. *)
   let in_lines = In_channel.with_open_text file In_channel.input_lines in
   let input = List.hd in_lines in
   let disk = parse_disk 0 input in
   List.iter (fun i -> Printf.printf "%i " i) disk;
+  let defrag_disk = defragment disk in
+  Printf.printf "\n";
+  List.iter (fun i -> Printf.printf "%i " i) defrag_disk;
+  let ck = checksum defrag_disk in
+  Printf.printf "\nChecksum: %i\n" ck;
   flush stdout
