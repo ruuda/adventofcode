15933106be9644210447b966af67d1291a1d0aab
Ruud van Asseldonk <dev@veniogames.com>
2024-12-10 09:52:01 +0100

    Solve 2024-10a in SQLite

--- 2024/10/main.sql
+++ 2024/10/main.sql
@@ -2,8 +2,62 @@
 
 -- Import the file with numbered lines.
 create table lines (heights text);
-.import --csv 'example1.txt' lines
+.import --csv 'input.txt' lines
 create table y_lines (y integer primary key autoincrement, heights text);
 insert into y_lines (heights) select heights from lines;
 
-select * from y_lines;
+-- Expand the lines into a table with x-y coordinates and heights.
+create table topomap (x integer, y integer, h integer);
+create index ix_topomap_x on topomap (x);
+create index ix_topomap_y on topomap (y);
+with xs (x) as (
+  select 1
+  union all
+  select x + 1 from xs
+  limit (select length(heights) from y_lines where y = 1)
+)
+insert into
+  topomap (x, y, h)
+select
+  y, x, substring(heights, x, 1) as h
+from
+  y_lines, xs;
+
+-- Find all the start positions.
+create table trailheads (x integer, y integer);
+insert into trailheads (x, y) select x, y from topomap where h = 0;
+
+-- Build a table of all trail steps. Trail steps are identified by their
+-- position, the trailhead they started at, and they have a height.
+with trails (hx, hy, x, y, h) as (
+  select
+    h.x as hx, h.y as hy, h.x as x, h.y as y, m.h as h
+  from
+    trailheads h, topomap m
+  where
+    m.x = h.x and m.y = h.y
+  union 
+  select
+    t.hx, t.hy, m.x as x, m.y as y, m.h as h
+  from
+    trails t, topomap m
+  where
+    (m.h = t.h + 1) and (
+      false
+      or ((t.x = m.x + 1) and (t.y = m.y))
+      or ((t.x = m.x - 1) and (t.y = m.y))
+      or ((t.x = m.x) and (t.y = m.y + 1))
+      or ((t.x = m.x) and (t.y = m.y - 1))
+    )
+),
+trail_scores as (
+  select
+    hx, hy, count(*) as score
+  from
+    trails
+  where
+    h = 9
+  group by
+    hx, hy
+)
+select sum(score) from trail_scores;
