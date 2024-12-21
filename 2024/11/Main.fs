0c0e7c408548a772bec076d6a4006b541f985aa4
Ruud van Asseldonk <dev@veniogames.com>
2024-12-21 13:53:04 +0100

    Fix overflow, solve 2024-11b in FSharp
    I don't think I need the bigint, the overflow was in the count, but
    still, it works so let's keep it.

--- 2024/11/Main.fs
+++ 2024/11/Main.fs
@@ -4,6 +4,7 @@ module Main
 
 open System
 open System.Collections.Generic
+open System.Numerics
 
 let input = System.IO.File.ReadAllText("input.txt").Trim()
 
@@ -13,9 +14,9 @@ let cut : String -> String = fun s ->
     | st -> st
 
 // Return the number of stones after n blinks, when starting with `stone`.
-let rec blinkn : Dictionary<string, int> array -> int -> String -> int = fun cache n stone ->
+let rec blinkn : Dictionary<string, UInt64> array -> int -> String -> UInt64 = fun cache n stone ->
   match n with
-    | 0 -> 1
+    | 0 -> 1UL
     | _ ->
       if cache[n].ContainsKey(stone) then
         cache[n].Item(stone)
@@ -29,13 +30,13 @@ let rec blinkn : Dictionary<string, int> array -> int -> String -> int = fun cac
                 (blinkn cache (n - 1) (stone.Substring(0, k / 2))) +
                   (blinkn cache (n - 1) (cut (stone.Substring(k / 2, k / 2))))
               else
-                blinkn cache (n - 1) ((System.UInt64.Parse(stone) * 2024UL).ToString())
+                blinkn cache (n - 1) ((BigInteger.Parse(stone) * BigInteger(2024L)).ToString())
         cache[n].Add(stone, result)
         result
 
 [<EntryPoint>]
 let main args =
-  let cache = Array.init 76 (fun _ -> new Dictionary<string, int>())
+  let cache = Array.init 76 (fun _ -> new Dictionary<string, UInt64>())
   let result = input.Split(" ") |> Array.map (blinkn cache 75) |> Array.sum
   printfn "%i" result
   0
