d81a28fac09267032402fd622a87522bf6c65da8
Ruud van Asseldonk <dev@veniogames.com>
2024-12-25 14:49:23 +0100

    Try to write a move function in Racket

--- 2024/15/main.rkt
+++ 2024/15/main.rkt
@@ -21,6 +21,40 @@
 (define territory (reverse territory-list))
 (define moves (string-join (reverse moves-list) ""))
 
+(define (lookup board x y)
+  (string-ref (list-ref board y) x))
 
+(define (put-at x y v board)
+  ; TODO board
+  board)
+
+; Move the thing at position (x, y) in the direction `dir` if possible; returns
+; whether position (x, y) is now empty, and mutates the board.
+(define (move board x y dir)
+  (define-values (nx ny)
+    (cond
+      [(equal? dir "^") (values x (- y 1))]
+      [(equal? dir "v") (values x (+ y 1))]
+      [(equal? dir "<") (values (- x 1) y)]
+      [(equal? dir ">") (values (+ x 1) y)]))
+  (let
+    ([at-pos (lookup (unbox board) x y)]
+     [at-npos (lookup (unbox board) nx ny)])
+    (cond
+      ; Walls can't move, and can't be moved into.
+      [(equal? at-pos #\#) #f]
+      [(equal? at-npos #\#) #f]
+      ; Empty spaces have no need to move.
+      [(equal? at-pos #\.) #f]
+      ; If it's not # or . then it must be a box (O) or player (@).
+      ; If the space is empty they can definitely move.
+      [(equal? at-npos #\.)
+       (set-box! board (put-at x y #\. (put-at nx ny at-pos (unbox board))))
+       #t]
+      ; If the space was not empty, then we can try to make space there.
+      [(move board nx ny dir)
+       (set-box! board (put-at x y #\. (put-at nx ny at-pos (unbox board))))
+       #t]
+      [else #f])))
 
 (print moves)
