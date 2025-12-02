ea8144737e926f58ecb8b559c1640b727576741e
Ruud van Asseldonk <dev@veniogames.com>
2025-12-02 10:10:12 +0100

    Solve 2025-02b in Zig

--- 2025/02/main.zig
+++ 2025/02/main.zig
@@ -34,34 +34,65 @@ fn read_input(fname: []const u8) ![]Range {
     return data.items;
 }
 
-fn count_duplicates(low: u64, high: u64) u64 {
-    const min_digits: u64 = (std.math.log10_int(low) + 1) / 2;
-    const max_digits: u64 = (std.math.log10_int(high) + 1) / 2;
-
-    var n_digits = min_digits;
+fn count_duplicates(comptime max_parts: u32, low: u64, high: u64) !u64 {
+    print("{} - {}\n", .{ low, high });
+    var n_parts: u32 = 2;
     var sum: u64 = 0;
 
-    while (n_digits <= max_digits) {
-        const i_min = std.math.pow(u64, 10, n_digits -| 1);
-        const i_max = std.math.pow(u64, 10, n_digits);
-        print("  {} digits, checking: {} - {}\n", .{ n_digits, i_min, i_max });
-        var i = i_min;
+    // Now that we check multiple parts, we may visit duplicates, so we need to
+    // exclude those.
+    var buf: [256]u64 = undefined;
+    var discoveries = std.ArrayList(u64).initBuffer(&buf);
 
-        // TODO: We can tighten the lower bound.
+    while (n_parts <= max_parts) {
+        const min_digits: u64 = (std.math.log10_int(low) + 1) / n_parts;
+        const max_digits: u64 = (std.math.log10_int(high) + 1) / n_parts;
 
-        while (i < i_max) {
-            const dupdup = i + i * i_max;
-            if (dupdup < low) {
+        var n_digits = if (min_digits == 0) 1 else min_digits;
+
+        while (n_digits <= max_digits) {
+            const i_min = std.math.pow(u64, 10, n_digits -| 1);
+            const i_max = std.math.pow(u64, 10, n_digits);
+            print("     {} parts {} digits, checking: {} - {}\n", .{ n_parts, n_digits, i_min, i_max });
+            var i = i_min;
+
+            while (i < i_max) {
+                // Repeat the number n_parts times in decimal.
+                var dupdup = i;
+                var j: u32 = 1;
+                while (j < n_parts) {
+                    dupdup = dupdup * i_max + i;
+                    j += 1;
+                }
+                if (dupdup < low) {
+                    i += 1;
+                    continue;
+                }
+                if (dupdup > high) break;
+
+                var is_new = true;
+                for (discoveries.items) |other| {
+                    if (other == dupdup) {
+                        is_new = false;
+                        break;
+                    }
+                }
+
+                if (is_new) {
+                    sum += dupdup;
+                    try discoveries.appendBounded(dupdup);
+                    print("  => {} in {}-{}\n", .{ dupdup, low, high });
+                } else {
+                    print("  =x {} in {}-{} (skip duplicate)\n", .{ dupdup, low, high });
+                }
+
                 i += 1;
-                continue;
             }
-            if (dupdup > high) break;
-            print("  found {} in {}-{}\n", .{ dupdup, low, high });
-            sum += dupdup;
-            i += 1;
+
+            n_digits += 1;
         }
 
-        n_digits += 1;
+        n_parts += 1;
     }
 
     return sum;
@@ -69,10 +100,19 @@ fn count_duplicates(low: u64, high: u64) u64 {
 
 pub fn main() !void {
     const data = try read_input("input.txt");
-    var sum: u64 = 0;
-    for (data) |item| {
-        print("{}-{}\n", .{ item.low, item.high });
-        sum += count_duplicates(item.low, item.high);
+
+    if (false) {
+        var part1: u64 = 0;
+        for (data) |item| {
+            part1 += try count_duplicates(2, item.low, item.high);
+        }
+        print("Part 1 sum: {}\n", .{part1});
     }
-    print("Sum: {}\n", .{sum});
+
+    var part2: u64 = 0;
+    for (data) |item| {
+        // u64::MAX has 20 decimal digits, so we can have at most 20 parts.
+        part2 += try count_duplicates(20, item.low, item.high);
+    }
+    print("Part 2 sum: {}\n", .{part2});
 }
