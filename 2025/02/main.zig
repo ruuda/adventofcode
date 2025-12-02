b92b8d85c20c8614606a4f51275f485dfd9cc0fd
Ruud van Asseldonk <dev@veniogames.com>
2025-12-02 08:59:42 +0100

    Struggle to read a file in Zig 0.15.2
    The docs are very scattered, but I suppose I only have to figure this
    stuff out once and then I can copy-paste it later.

---
+++ 2025/02/main.zig
@@ -0,0 +1,37 @@
+const std = @import("std");
+const print = @import("std").debug.print;
+
+const Range = struct {
+    low: u64,
+    high: u64,
+};
+
+fn read_input(fname: []const u8) ![]Range {
+    const allocator = std.heap.page_allocator;
+    var data = try std.ArrayList(Range).initCapacity(allocator, 128);
+
+    const file = try std.fs.cwd().openFile(fname, .{});
+    defer file.close();
+
+    var buf: [256]u8 = undefined;
+    var reader = file.reader(&buf);
+
+    while (true) {
+        const part = try reader.interface.takeDelimiterExclusive(',');
+        if (part.len == 0) break;
+        var part_iter = std.mem.splitScalar(u8, part, '-');
+        const low = try std.fmt.parseInt(u64, part_iter.first(), 10);
+        const high = try std.fmt.parseInt(u64, part_iter.next().?, 10);
+
+        try data.append(allocator, Range{ .low = low, .high = high });
+    }
+
+    return data.items;
+}
+
+pub fn main() !void {
+    const data = try read_input("example.txt");
+    for (data) |item| {
+        print("{}-{}", .{ item.low, item.high });
+    }
+}
