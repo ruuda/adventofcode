40f19f005106821257365f19ca6b98f4d71cb2d9
Ruud van Asseldonk <dev@veniogames.com>
2025-12-04 09:24:29 +0100

    Solve 2025-04a in Zig

--- 2025/04/main.zig
+++ 2025/04/main.zig
@@ -21,8 +21,60 @@ fn read_input(fname: []const u8) ![][]u8 {
     return data.items;
 }
 
-pub fn main() !void {
-    const data = try read_input("example.txt");
+fn count_adjacent(map: [][]u8) u32 {
+    const w = map[0].len;
+    const h = map.len;
+    const dp = [3]i32{ -1, 0, 1 };
+    var count: u32 = 0;
 
-    print("data: {s}\n", .{data[0]});
+    for (0..h) |y| {
+        const iy: i32 = @intCast(y);
+        for (0..w) |x| {
+            const ix: i32 = @intCast(x);
+
+            // We are only interested in cells that contain a roll ('@'),
+            // empty cells we can skip.
+            if (map[y][x] == '.') {
+                print(".", .{});
+                continue;
+            }
+
+            var num_adjacent: u32 = 0;
+
+            for (dp) |dx| {
+                for (dp) |dy| {
+                    if (dx == 0 and dy == 0) continue;
+                    const px = ix + dx;
+                    const py = iy + dy;
+                    var p: u8 = '.';
+                    if (py >= 0 and py < map.len) {
+                        const line = map[@intCast(py)];
+                        if (px >= 0 and px < line.len) {
+                            p = line[@intCast(px)];
+                        }
+                    }
+
+                    num_adjacent += if (p == '@') 1 else 0;
+                }
+            }
+            print("{}", .{num_adjacent});
+
+            count += if (num_adjacent < 4) 1 else 0;
+        }
+        print("\n", .{});
+    }
+
+    return count;
+}
+
+pub fn main() !void {
+    const data = try read_input("input.txt");
+
+    for (data) |line| {
+        print("{s}\n", .{line});
+    }
+    print("\n\n", .{});
+
+    const part1 = count_adjacent(data);
+    print("{}\n", .{part1});
 }
