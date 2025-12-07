02e0c6715c3de589e6d09e84a915942f65954fb2
Ruud van Asseldonk <dev@veniogames.com>
2025-12-07 10:33:33 +0100

    I don't understand part 2
    I think it is asking for the number of possible routes that reach the
    bottom row, and I think I am computing that correctly, but the number in
    the example, 40, is not the number of possible routes. What am I missing?

--- 2025/07/main.zig
+++ 2025/07/main.zig
@@ -21,22 +21,47 @@ fn readInput(fname: []const u8) ![][]u8 {
     return data.items;
 }
 
-fn countSplits(map: [][]u8) u64 {
+const Result = struct {
+    nSplits: u64,
+    nTimelines: u64,
+};
+
+fn countSplits(map: [][]u8) !Result {
     var nSplits: u64 = 0;
+
+    // For every beam location, we will track the number of distinct
+    // ways to get there.
+    const allocator = std.heap.page_allocator;
+    var nPaths = try std.ArrayList([]u64).initCapacity(allocator, map.len);
+
+    // The size of `nPaths` must match `map` so put something on the first line,
+    // we don't reference it.
+    nPaths.appendAssumeCapacity(try allocator.alloc(u64, map[0].len));
 
     for (1..map.len) |y| {
         const rowPrev = map[y - 1];
         var rowCurr = map[y];
+
+        var paths = try allocator.alloc(u64, rowCurr.len);
+        @memset(paths, 0);
+        nPaths.appendAssumeCapacity(paths);
+
         for (0..map[y].len) |x| {
             const above = rowPrev[x];
             const cell = rowCurr[x];
             if (above == 'S' or above == '|') {
                 // There is a beam, extend it downwards.
+                const p = if (above == 'S') 1 else nPaths.items[y - 1][x];
                 switch (cell) {
-                    '.' => rowCurr[x] = '|',
+                    '.' => {
+                        rowCurr[x] = '|';
+                        paths[x] += p;
+                    },
                     '^' => {
                         rowCurr[x - 1] = '|';
                         rowCurr[x + 1] = '|';
+                        paths[x - 1] += p;
+                        paths[x + 1] += p;
                         nSplits += 1;
                     },
                     '|' => {
@@ -46,13 +71,27 @@ fn countSplits(map: [][]u8) u64 {
                 }
             }
         }
+        for (paths) |pp| {
+            if (pp > 0) {
+                print("{:2} ", .{pp});
+            } else {
+                print("   ", .{});
+            }
+        }
+        print("\n", .{});
     }
 
-    return nSplits;
+    // For each of the reachable final beam positions, sum the number of ways to
+    // reach it, which is the answer to part 2.
+    var nTimelines: u64 = 0;
+    for (nPaths.items[nPaths.items.len - 1]) |count| nTimelines += count;
+
+    return Result{ .nSplits = nSplits, .nTimelines = nTimelines };
 }
 
 pub fn main() !void {
-    const data = try readInput("input.txt");
-    const part1 = countSplits(data);
-    print("Part 1: {}\n", .{part1});
+    const data = try readInput("example.txt");
+    const result = try countSplits(data);
+    print("Part 1: {}\n", .{result.nSplits});
+    print("Part 2: {}\n", .{result.nTimelines});
 }
