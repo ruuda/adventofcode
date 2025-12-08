e366866fe169faf56d0ccdef932cb1362f9d1c67
Ruud van Asseldonk <dev@veniogames.com>
2025-12-08 08:18:22 +0100

    Sort the pairs

--- 2025/08/main.zig
+++ 2025/08/main.zig
@@ -3,6 +3,15 @@ const print = @import("std").debug.print;
 
 const Point = struct {
     coord: [3]f32,
+
+    fn sqr_dist(self: Point, other: Point) f32 {
+        var res: f32 = 0.0;
+        inline for (0..3) |i| {
+            const d = self.coord[i] - other.coord[i];
+            res += d * d;
+        }
+        return res;
+    }
 };
 
 fn readInput(fname: []const u8) ![]Point {
@@ -31,9 +40,49 @@ fn readInput(fname: []const u8) ![]Point {
     return data.items;
 }
 
+const Pair = struct {
+    d2: f32,
+    ia: usize,
+    ib: usize,
+};
+
+fn ltPair(ctx: u64, lhs: Pair, rhs: Pair) bool {
+    _ = ctx;
+    if (lhs.d2 < rhs.d2) return true;
+    if (lhs.d2 > rhs.d2) return false;
+    if (lhs.ia < rhs.ia) return true;
+    if (lhs.ia > rhs.ia) return false;
+    return lhs.ib < lhs.ib;
+}
+
+fn countClusters(comptime nWires: u64, points: []Point) !u64 {
+    const allocator = std.heap.page_allocator;
+    // Yep it's quadratic, but for only 1000 pairs we don't really need to bring
+    // out the spatial data structures.
+    var pairs = try std.ArrayList(Pair).initCapacity(allocator, points.len * points.len / 2);
+    for (0..points.len) |i| {
+        for (i + 1..points.len) |j| {
+            const pair = Pair{
+                .d2 = points[i].sqr_dist(points[j]),
+                .ia = i,
+                .ib = j,
+            };
+            pairs.appendAssumeCapacity(pair);
+        }
+    }
+
+    const dummyContext: u64 = 0;
+    std.sort.pdq(Pair, pairs.items, dummyContext, ltPair);
+
+    for (pairs.items) |p| {
+        print("{} {} {}\n", .{ p.d2, p.ia, p.ib });
+    }
+
+    return 0 + nWires;
+}
+
 pub fn main() !void {
     const data = try readInput("example.txt");
-    for (data) |p| {
-        print("{} {} {}\n", .{ p.coord[0], p.coord[1], p.coord[2] });
-    }
+    const part1 = try countClusters(10, data);
+    print("Part 1: {}\n", .{part1});
 }
